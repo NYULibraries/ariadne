3e3ea880c0ea07783643208d5c8fe731d2b58976
David <da70@nyu.edu>
2022-08-24 22:03:29 -0400

    backend/server_test.go: set up test case loop

--- backend/server_test.go
+++ backend/server_test.go
@@ -12,6 +12,11 @@ import (
 	"testing"
 )
 
+type TestCase struct {
+	name   				string
+	queryString			string
+}
+
 //go:embed all:testdata
 var testdataFS embed.FS
 
@@ -26,20 +31,25 @@ func TestMain(m *testing.M) {
 }
 
 func TestResponseJSONRoute(t *testing.T) {
-	var err error
+	var currentTestCase TestCase
 
-	sfxFakeResponse, err := getTestdataFileContents("testdata/server/fixtures/sfx-fake-responses/new-yorker.xml")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expectedJSON, err := getTestdataFileContents("testdata/server/golden/new-yorker.golden.json")
-	if err != nil {
-		t.Fatal(err)
+	testCases := []TestCase{
+		{
+			name: "new-yorker",
+			queryString: "url_ver=Z39.88-2004&url_ctx_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Actx&ctx_ver=Z39.88-2004&ctx_tim=2021-10-22T12%3A29%3A27-04%3A00&ctx_id=&ctx_enc=info%3Aofi%2Fenc%3AUTF-8&rft.aulast=Ross&rft.date=2002&rft.eissn=2163-3827&rft.genre=journal&rft.issn=0028-792X&rft.jtitle=New+Yorker&rft.language=eng&rft.lccn=++2011201780&rft.object_id=110975413975944&rft.oclcnum=909782404&rft.place=New+York&rft.private_data=909782404<fssessid>0<%2Ffssessid>&rft.pub=F-R+Pub.+Corp.&rft.stitle=NEW+YORKER&rft.title=New+Yorker&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft_id=info%3Aoclcnum%2F909782404&rft_id=urn%3AISSN%3A0028-792X&req.ip=209.150.44.95&rfr_id=info%3Asid%2FFirstSearch%3AWorldCat",
+		},
 	}
 
 	fakeSFXServer := httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			sfxFakeResponseFile := "testdata/server/fixtures/sfx-fake-responses/" +
+				currentTestCase.name +
+				".xml"
+			sfxFakeResponse, err := getTestdataFileContents(sfxFakeResponseFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+
 			fmt.Fprint(w, sfxFakeResponse)
 		}),
 	)
@@ -47,25 +57,39 @@ func TestResponseJSONRoute(t *testing.T) {
 
 	sfx.SetSFXURL(fakeSFXServer.URL)
 
-	responseRecorder := httptest.NewRecorder()
-
-	request, err := http.NewRequest(
-		"GET",
-		"/v0/?url_ver=Z39.88-2004&url_ctx_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Actx&ctx_ver=Z39.88-2004&ctx_tim=2021-10-22T12%3A29%3A27-04%3A00&ctx_id=&ctx_enc=info%3Aofi%2Fenc%3AUTF-8&rft.aulast=Ross&rft.date=2002&rft.eissn=2163-3827&rft.genre=journal&rft.issn=0028-792X&rft.jtitle=New+Yorker&rft.language=eng&rft.lccn=++2011201780&rft.object_id=110975413975944&rft.oclcnum=909782404&rft.place=New+York&rft.private_data=909782404%3Cfssessid%3E0%3C%2Ffssessid%3E&rft.pub=F-R+Pub.+Corp.&rft.stitle=NEW+YORKER&rft.title=New+Yorker&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft_id=info%3Aoclcnum%2F909782404&rft_id=urn%3AISSN%3A0028-792X&req.ip=209.150.44.95&rfr_id=info%3Asid%2FFirstSearch%3AWorldCat",
-		nil,
-	)
-	if err != nil {
-		t.Fail()
-	}
-
 	router := NewRouter()
-	router.ServeHTTP(responseRecorder, request)
 
-	response := responseRecorder.Result()
-	body, _ := io.ReadAll(response.Body)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			currentTestCase = testCase
 
-	if string(body) != expectedJSON {
-		t.Errorf(expectedJSON)
+			request, err := http.NewRequest(
+				"GET",
+				"/v0/?" + testCase.queryString,
+				nil,
+			)
+			if err != nil {
+				t.Fail()
+			}
+
+			responseRecorder := httptest.NewRecorder()
+			router.ServeHTTP(responseRecorder, request)
+
+			response := responseRecorder.Result()
+			body, _ := io.ReadAll(response.Body)
+
+			goldenFileName := "testdata/server/golden/" +
+				testCase.name +
+				".golden.json"
+			goldenJSON, err := getTestdataFileContents(goldenFileName)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if string(body) != goldenJSON {
+				t.Errorf(goldenJSON)
+			}
+		})
 	}
 }
 
