0db81fa6aca3602ca7176de5ca217c7f62b56967
David <da70@nyu.edu>
2022-09-12 10:33:46 -0400

    backend/: add util.Diff and print diff when golden vs. actual values are not equal

---
+++ backend/util/util.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"os/exec"
+)
+
+func Diff(path1 string, path2 string) (string, error) {
+	diffCmd := "diff"
+
+	outputBytes, err := exec.Command(diffCmd, "-r", path1, path2).CombinedOutput()
+	if err != nil {
+		switch err.(type) {
+			case *exec.ExitError:
+				// `diff` ran successfully with non-zero exit code.  Report the
+				// differences.
+			default:
+				// `diff` command failed to run.
+				return "", err
+		}
+	}
+
+	return string(outputBytes), nil
+}
