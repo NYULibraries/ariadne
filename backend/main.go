7942aebd10b063a3208ee92229b7027c99b17b80
David <da70@nyu.edu>
2022-09-20 19:47:29 -0400

    Move server{_test}.go to new package api/

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"resolve/api"
 )
 
 // Run on port 8080
 const appPort = "8080"
 
 func main() {
-	router := NewRouter()
+	router := api.NewRouter()
 
 	log.Println("Listening on port", appPort)
 	log.Fatal(http.ListenAndServe(":"+appPort, router))
