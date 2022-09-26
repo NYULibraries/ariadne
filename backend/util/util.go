a2e566aa5a1d289541e408a65b728b6d9095a96d
David <da70@nyu.edu>
2022-09-26 19:16:15 -0400

    Run `gofmt -w ./backend/`

--- backend/util/util.go
+++ backend/util/util.go
@@ -10,12 +10,12 @@ func Diff(path1 string, path2 string) (string, error) {
 	outputBytes, err := exec.Command(diffCmd, "-r", path1, path2).CombinedOutput()
 	if err != nil {
 		switch err.(type) {
-			case *exec.ExitError:
-				// `diff` ran successfully with non-zero exit code.  Report the
-				// differences.
-			default:
-				// `diff` command failed to run.
-				return "", err
+		case *exec.ExitError:
+			// `diff` ran successfully with non-zero exit code.  Report the
+			// differences.
+		default:
+			// `diff` command failed to run.
+			return "", err
 		}
 	}
 
