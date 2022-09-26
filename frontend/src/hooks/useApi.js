44f01ec715ae8655528cd8a4f486bac2abbae9b6
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-09-26 17:54:33 -0400

    useApi.js: added lastElement, error to state

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 const useApi = (url, query) => {
   const [elements, setElements] = useState([]);
+  const [error, setError] = useState(null);
+  const [lastElement, setLastElement] = useState(null);
 
   const getElements = async () => {
     try {
@@ -9,12 +11,13 @@ const useApi = (url, query) => {
       const jsonData = await response.json();
       let arrOfLinks = jsonData.ctx_obj[0].ctx_obj_targets[0].target;
       setElements(arrOfLinks.slice(0, -1));
-    } catch (err) {
-      console.error(err.message);
+      setLastElement(arrOfLinks.at(-1));
+    } catch (error) {
+      setError(error.message || 'Something went wrong');
     }
   };
 
-  return { elements, getElements };
+  return { elements, getElements, lastElement, error };
 };
 
 export default useApi;
