483ac879150e14ba0dc3b128ddc4f9e8671dfb0f
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-09-27 10:26:11 -0400

    List.js: refactored to reflect changes in useApi

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -3,14 +3,10 @@ import { useEffect } from 'react';
 import useApi from '../../hooks/useApi';
 
 const List = () => {
-  const baseURl = process.env.REACT_APP_API_URL;
-  const query =
-    '?url_ver=Z39.88-2004&url_ctx_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Actx&ctx_ver=Z39.88-2004&ctx_tim=2021-10-22T12%3A29%3A27-04%3A00&ctx_id=&ctx_enc=info%3Aofi%2Fenc%3AUTF-8&rft.aulast=Ross&rft.date=2002&rft.eissn=2163-3827&rft.genre=journal&rft.issn=0028-792X&rft.jtitle=New+Yorker&rft.language=eng&rft.lccn=++2011201780&rft.object_id=110975413975944&rft.oclcnum=909782404&rft.place=New+York&rft.private_data=909782404%3Cfssessid%3E0%3C%2Ffssessid%3E&rft.pub=F-R+Pub.+Corp.&rft.stitle=NEW+YORKER&rft.title=New+Yorker&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft_id=info%3Aoclcnum%2F909782404&rft_id=urn%3AISSN%3A0028-792X&req.ip=209.150.44.95&rfr_id=info%3Asid%2FFirstSearch%3AWorldCat';
-
-  const { elements, getElements, lastElement, error } = useApi(baseURl, query);
+  const getLinksApi = useApi();
 
   useEffect(() => {
-    getElements();
+    getLinksApi.request();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -24,42 +20,41 @@ const List = () => {
       </div>
       <div className="i-am-centered">
         <div className="list-group">
-          {elements &&
-            elements.map((element, idx) => (
-              <div key={idx} className="list-group-item list-group-item-action flex-column" style={{ border: 'none' }}>
-                <div className="d-flex w-100 justify-content-between">
-                  <h6 className="mb-1">
-                    <a
-                      style={{ textDecoration: 'none', color: '#6c07ae' }}
-                      href={element.target_url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {element.target_public_name}
-                    </a>
-                  </h6>
-                  <small>{element.coverage[0].coverage_text[0].threshold_text[0].coverage_statement}</small>
-                </div>
+          {getLinksApi.elements?.map((element, idx) => (
+            <div key={idx} className="list-group-item list-group-item-action flex-column" style={{ border: 'none' }}>
+              <div className="d-flex w-100 justify-content-between">
+                <h6 className="mb-1">
+                  <a
+                    style={{ textDecoration: 'none', color: '#6c07ae' }}
+                    href={element.target_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {element.target_public_name}
+                  </a>
+                </h6>
+                <small>{element.coverage[0].coverage_text[0].threshold_text[0].coverage_statement}</small>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
-        {lastElement && (
+        {getLinksApi.lastElement && (
           <div className="ask-librarian">
             <h6>
               <a
                 style={{ textDecoration: 'none', color: '#6c07ae' }}
-                href={lastElement.target_url}
+                href={getLinksApi.lastElement.target_url}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {lastElement.target_public_name}
+                {getLinksApi.lastElement.target_public_name}
               </a>
             </h6>
           </div>
         )}
       </div>
 
-      {error && <div className="i-am-centered">{error}</div>}
+      {getLinksApi.error && <div className="i-am-centered">{getLinksApi.error}</div>}
     </>
   );
 };
