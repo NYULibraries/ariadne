161d55ee8cb76679d25c927497f08987397ae1c2
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-09-27 16:37:28 -0400

    List.test.js: more tests added

--- frontend/src/components/List/List.test.js
+++ frontend/src/components/List/List.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import List from './List';
 
 test('renders with a className of list-group', () => {
@@ -8,11 +8,6 @@ test('renders with a className of list-group', () => {
 
 test('renders correctly', () => {
   const { container } = render(<List />);
-  expect(container.firstChild).toMatchSnapshot();
-});
-
-test('renders Loading... when loading', () => {
-  const { container } = render(<List loading />);
   expect(container.firstChild).toMatchSnapshot();
 });
 
@@ -34,4 +29,33 @@ test('renders a last element', () => {
 test('renders a list of elements and a last element', () => {
   const { container } = render(<List elements={[{ id: 1 }, { id: 2 }]} lastElement={{ id: 3 }} />);
   expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders Loading...', () => {
+  render(<List />);
+  const linkElement = screen.getByText(/Loading/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a E Journal Full Text link', async () => {
+  render(<List />);
+  const linkElement = await waitFor(() => screen.getByText(/E Journal Full Text/i));
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a Gale General OneFile link', async () => {
+  render(<List />);
+  const linkElement = await waitFor(() => screen.getByText(/Gale General OneFile/i));
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Ask a Librarian', async () => {
+  render(<List />);
+  const linkElement = await waitFor(() => screen.getByText(/Ask a Librarian/i));
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Loading... no longer present in the DOM after loading data', async () => {
+  const { getByText } = render(<List />);
+  await waitForElementToBeRemoved(() => getByText(/Loading/i));
 });
