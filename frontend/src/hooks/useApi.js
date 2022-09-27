bf07252d53f5d795466f7da44df6ce40641d63c9
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-09-27 10:25:30 -0400

    useApi: refactored to utilize getLinks fucntion

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
+import getLinks from '../api/getLinks';
 
-const useApi = (url, query) => {
+const useApi = () => {
   const [elements, setElements] = useState([]);
   const [error, setError] = useState(null);
   const [lastElement, setLastElement] = useState(null);
 
-  const getElements = async () => {
+  const request = async () => {
     try {
-      const response = await fetch(url + query);
-      const jsonData = await response.json();
+      const response = await getLinks();
+      const jsonData = await response.data;
       let arrOfLinks = jsonData.ctx_obj[0].ctx_obj_targets[0].target;
       setElements(arrOfLinks.slice(0, -1));
       setLastElement(arrOfLinks.at(-1));
@@ -17,7 +18,7 @@ const useApi = (url, query) => {
     }
   };
 
-  return { elements, getElements, lastElement, error };
+  return { elements, request, lastElement, error };
 };
 
 export default useApi;
