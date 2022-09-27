1fe6dc685a19993c6d593818c7b094f7d28937dc
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2022-09-27 15:11:44 -0400

    List.test.js: added more tests; also frontend/README.md removed

--- frontend/src/components/List/List.test.js
+++ frontend/src/components/List/List.test.js
@@ -10,3 +10,28 @@ test('renders correctly', () => {
   const { container } = render(<List />);
   expect(container.firstChild).toMatchSnapshot();
 });
+
+test('renders Loading... when loading', () => {
+  const { container } = render(<List loading />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders an error when error', () => {
+  const { container } = render(<List error="Error" />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders a list of elements', () => {
+  const { container } = render(<List elements={[{ id: 1 }, { id: 2 }]} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders a last element', () => {
+  const { container } = render(<List lastElement={{ id: 1 }} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders a list of elements and a last element', () => {
+  const { container } = render(<List elements={[{ id: 1 }, { id: 2 }]} lastElement={{ id: 3 }} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
