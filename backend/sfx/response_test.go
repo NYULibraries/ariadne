f3d9a8ff66a3920b68b003e56e53029130900027
David <da70@nyu.edu>
2022-10-26 18:45:53 -0400

    response_test.go: convert TestToResponseJson to TestNewMultipleObjectsResponse

--- backend/sfx/response_test.go
+++ backend/sfx/response_test.go
@@ -1,63 +1,75 @@
 package sfx
 
-// TODO: Redo this test so that it works with new MultiObjectsResponse type, which
-// has a JSON field that needs to be tested.  `toResponseJSON` has been deleted.
-//func TestToResponseJson(t *testing.T) {
-//	dummyGoodXMLResponse := `
-//<ctx_obj_set>
-//	<ctx_obj>
-//		<ctx_obj_targets>
-//			<target>
-//				<target_url>http://answers.library.newschool.edu/</target_url>
-//			</target>
-//		</ctx_obj_targets>
-//	</ctx_obj>
-//</ctx_obj_set>`
-//	dummyBadXMLResponse := `
-//<ctx_obj_set`
-//	dummyJSONResponse := `{
-//    "ctx_obj": [
-//        {
-//            "ctx_obj_targets": [
-//                {
-//                    "target": [
-//                        {
-//                            "target_name": "",
-//                            "target_public_name": "",
-//                            "target_url": "http://answers.library.newschool.edu/",
-//                            "authentication": "",
-//                            "proxy": ""
-//                        }
-//                    ]
-//                }
-//            ]
-//        }
-//    ]
-//}`
-//	var tests = []struct {
-//		from        []byte
-//		expected    string
-//		expectedErr error
-//	}{
-//		{[]byte(dummyGoodXMLResponse), dummyJSONResponse, nil},
-//		{[]byte(dummyBadXMLResponse), "", errors.New("error")},
-//	}
-//
-//	for _, tt := range tests {
-//		testname := fmt.Sprintf("%s", tt.expected)
-//		t.Run(testname, func(t *testing.T) {
-//			ans, err := toResponseJSON(tt.from)
-//			// if err != nil {
-//			// 	t.Errorf("error %v", err)
-//			// }
-//			if tt.expectedErr != nil {
-//				if err == nil {
-//					t.Errorf("toResponseJSON err was '%v', expecting '%v'", err, tt.expectedErr)
-//				}
-//			}
-//			if ans != tt.expected {
-//				t.Errorf("toResponseJSON was '%v', expecting '%v'", ans, tt.expected)
-//			}
-//		})
-//	}
-//}
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+const dummyGoodXMLResponse = `
+<ctx_obj_set>
+	<ctx_obj>
+		<ctx_obj_targets>
+			<target>
+				<target_url>http://answers.library.newschool.edu/</target_url>
+			</target>
+		</ctx_obj_targets>
+	</ctx_obj>
+</ctx_obj_set>`
+
+const dummyBadXMLResponse = `
+<ctx_obj_set`
+
+const dummyJSONResponse = `{
+    "ctx_obj": [
+        {
+            "ctx_obj_targets": [
+                {
+                    "target": [
+                        {
+                            "target_name": "",
+                            "target_public_name": "",
+                            "target_url": "http://answers.library.newschool.edu/",
+                            "authentication": "",
+                            "proxy": ""
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}`
+
+func TestNewMultipleObjectsResponse(t *testing.T) {
+	testCases := []struct {
+		name          string
+		httpResponse  *http.Response
+		expected      string
+		expectedError error
+	}{
+		{"Good SFX response", makeFakeHTTPResponse(dummyGoodXMLResponse), dummyJSONResponse, nil},
+		{"Bad SFX response", makeFakeHTTPResponse(dummyBadXMLResponse), "", errors.New("error")},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			multipleObjectsResponse, err := newMultipleObjectsResponse(testCase.httpResponse)
+			if testCase.expectedError != nil {
+				if err == nil {
+					t.Errorf("newMultipleObjectsResponse error was '%v', expecting '%v'", err, testCase.expectedError)
+				}
+			}
+			if multipleObjectsResponse.JSON != testCase.expected {
+				t.Errorf("newMultipleObjectsResponse was '%v', expecting '%v'", multipleObjectsResponse.JSON, testCase.expected)
+			}
+		})
+	}
+}
+
+func makeFakeHTTPResponse(body string) *http.Response {
+	return &http.Response{
+		Body: ioutil.NopCloser(bytes.NewBufferString(body)),
+	}
+}
