079897130c82b040f025bef08d62747faaee15dd
David <da70@nyu.edu>
2022-11-11 09:28:34 -0500

    Fix `frontend-test` Docker Compose service

--- frontend/src/testutils/index.js
+++ frontend/src/testutils/index.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const path = require('path');
 
-const BACKEND_API_TESTDATA = path.join(
+// Prefer actual backend/api/testdata/ files if they exist (i.e. running tests on
+// local machine while working in the repo).  If in a container, the backend/api/testdata/
+// files will have been copied into this directory.
+const BACKEND_API_TESTDATA_DIR_IN_REPO = path.join(
     __dirname,
     '..',
     '..',
@@ -10,8 +14,16 @@ const BACKEND_API_TESTDATA = path.join(
     'testdata',
     'server',
 );
-const BACKEND_API_TEST_CASES_GOLDEN_FILES_DIR = path.join(BACKEND_API_TESTDATA, 'golden');
-const BACKEND_API_TEST_CASES_INDEX = path.join(BACKEND_API_TESTDATA, 'test-cases.json');
+const BACKEND_API_TESTDATA_DIR_IN_CONTAINER = path.join(
+    __dirname,
+    'backend-testdata',
+);
+const BACKEND_API_TESTDATA_DIR = fs.existsSync(BACKEND_API_TESTDATA_DIR_IN_REPO) ?
+                                 BACKEND_API_TESTDATA_DIR_IN_REPO :
+                                 BACKEND_API_TESTDATA_DIR_IN_CONTAINER;
+
+const BACKEND_API_TEST_CASES_GOLDEN_FILES_DIR = path.join(BACKEND_API_TESTDATA_DIR, 'golden');
+const BACKEND_API_TEST_CASES_INDEX = path.join(BACKEND_API_TESTDATA_DIR, 'test-cases.json');
 
 function getTestCases() {
     const testCasesFromAriadneBackendApi = require(BACKEND_API_TEST_CASES_INDEX);
