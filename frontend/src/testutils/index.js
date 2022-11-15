2b04a732ee59b4e3989d0843c4e08f082feff064
David <da70@nyu.edu>
2022-11-14 19:41:54 -0500

    frontend/src/testutils/index.js: change indentation from 4 spaces to 2 spaces to conform to existing style

--- frontend/src/testutils/index.js
+++ frontend/src/testutils/index.js
@@ -5,18 +5,18 @@ const path = require('path');
 // local machine while working in the repo).  If in a container, the backend/api/testdata/
 // files will have been copied into this directory.
 const BACKEND_API_TESTDATA_DIR_IN_REPO = path.join(
-    __dirname,
-    '..',
-    '..',
-    '..',
-    'backend',
-    'api',
-    'testdata',
-    'server',
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'backend',
+  'api',
+  'testdata',
+  'server',
 );
 const BACKEND_API_TESTDATA_DIR_IN_CONTAINER = path.join(
-    __dirname,
-    'backend-testdata',
+  __dirname,
+  'backend-testdata',
 );
 const BACKEND_API_TESTDATA_DIR = fs.existsSync(BACKEND_API_TESTDATA_DIR_IN_REPO) ?
                                  BACKEND_API_TESTDATA_DIR_IN_REPO :
@@ -28,17 +28,17 @@ const BACKEND_API_TEST_CASES_INDEX = path.join(BACKEND_API_TESTDATA_DIR, 'test-c
 function getTestCases() {
     const testCasesFromAriadneBackendApi = require(BACKEND_API_TEST_CASES_INDEX);
 
-    testCasesFromAriadneBackendApi.forEach( testCase => {
-        testCase.response = getResponse(testCase.key);
-    });
+  testCasesFromAriadneBackendApi.forEach(testCase => {
+    testCase.response = getResponse(testCase.key);
+  });
 
-    return testCasesFromAriadneBackendApi;
+  return testCasesFromAriadneBackendApi;
 }
 
 function getResponse(key) {
-    const goldenFile = path.join(BACKEND_API_TEST_CASES_GOLDEN_FILES_DIR, `${key}.json`);
+  const goldenFile = path.join(BACKEND_API_TEST_CASES_GOLDEN_FILES_DIR, `${key}.json`);
 
-    return require(goldenFile);
+  return require(goldenFile);
 }
 
 export {
