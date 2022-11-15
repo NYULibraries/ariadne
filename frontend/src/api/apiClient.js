c6f2c20433a10a496fa0c310864bfb27f6e65e8e
David <da70@nyu.edu>
2022-11-14 19:41:32 -0500

    frontend/: useApi and apiClient: enable handling of HTTP error responses and errors returned by a successful API response (i.e. HTTP 200 that returns errors in top-level `errors` array)

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,9 +1,7 @@
 const apiClient = {
   get: async function get(path) {
     const url = `${process.env.REACT_APP_API_URL}${path}`;
-    const response = await fetch(url);
-
-    return response.json();
+    return await fetch(url);
   },
 };
 
