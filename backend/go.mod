4ff8f4cbd148d6656e8a174a58341d1873610096
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-01-03 22:27:29 -0500

    util/util.go: InitSentry function added to the util package

--- backend/go.mod
+++ backend/go.mod
@@ -2,9 +2,14 @@ module ariadne
 
 go 1.17
 
-require github.com/spf13/cobra v1.6.1
+require (
+	github.com/getsentry/sentry-go v0.16.0
+	github.com/spf13/cobra v1.6.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- backend/util/util.go
+++ backend/util/util.go
@@ -1,7 +1,11 @@
 package util
 
 import (
+	"github.com/getsentry/sentry-go"
+	"log"
+	"os"
 	"os/exec"
+	"time"
 )
 
 func Diff(path1 string, path2 string) (string, error) {
@@ -20,4 +24,28 @@ func Diff(path1 string, path2 string) (string, error) {
 	}
 
 	return string(outputBytes), nil
+}
+
+func InitSentry() {
+	// Get the value of the SENTRY_DSN environment variable
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn == "" {
+		// SENTRY_DSN environment variable is not set
+		log.Println("SENTRY_DSN environment variable is not set")
+	} else {
+		// SENTRY_DSN environment variable is set
+		log.Println("SENTRY_DSN environment variable is set")
+		// Initialize the Sentry Go SDK and set up error reporting
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:   dsn,
+			Debug: true, // Enable debugging mode
+		})
+		if err != nil {
+			// Failed to initialize the Sentry SDK
+			log.Fatalf("sentry.Init: %s", err)
+		}
+		// Flush buffered events before the program terminates
+		defer sentry.Flush(2 * time.Second)
+	}
+	sentry.CaptureMessage("It works!")
 }
