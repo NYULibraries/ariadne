f61f895f65c70319cb75d80f038d172afc64793c
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-01-13 20:07:57 -0500

    frontend/src/components/Banner/Banner.js: image class is set depending on URL institution param

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -2,27 +2,28 @@ import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import shanghaiLogo from './images/shanghai-logo-color.svg';
-import abudhabiLogo from './images/abudhabi-logo-color.svg';
 
 const Banner = () => {
   const [logo, setLogo] = useState('https://cdn.library.nyu.edu/images/nyulibraries-logo.svg');
-  // const [institution, setInstitution] = useState(null);
+  const [link, setLink] = useState('http://library.nyu.edu');
+  const [imgClass, setImgClass] = useState('image');
 
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const institution = urlParams.get('institution');
     if (institution === 'NYUAD') {
-      setLogo(abudhabiLogo);
+      setLogo(`${process.env.PUBLIC_URL}/images/abudhabi-logo-color.svg`);
+      setLink('https://nyuad.nyu.edu/en/library.html');
+      setImgClass('image white-bg');
     } else if (institution === 'NYUSH') {
-      setLogo(shanghaiLogo);
+      setLogo(`${process.env.PUBLIC_URL}/images/shanghai-logo-color.svg`);
+      setLink('https://shanghai.nyu.edu/academics/library');
+      setImgClass('image white-bg');
     } else if (institution === 'umlaut.institution') {
-      // setInstitution(institution);
       urlParams.delete('umlaut.institution');
       window.location.search = urlParams.toString();
     }
-    // setInstitution(institution);
   }, []);
   return (
     <Navbar className="color-nav" expand="lg">
@@ -30,8 +31,8 @@ const Banner = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="http://library.nyu.edu">
-              <img className="image" src={logo} alt="NYU Libraries logo" width="220" height="30" />
+            <Nav.Link href={link}>
+              <img className={imgClass} src={logo} alt="NYU Libraries logo" width="220" height="60" />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
