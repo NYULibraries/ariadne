9d259b9b8ad2242fd2490df06a4d17d2a13b01ad
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-01-19 16:59:22 -0500

    frontend/src/components/Banner/Banner.js: importing default constants

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -3,11 +3,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { getQueryParameter, getInstitution } from '../../aux/helpers';
+import { DEFAULT_LOGO, DEFAULT_LINK, DEFAULT_IMG_CLASS } from '../../aux/institutionConstants';
 
 const Banner = () => {
-  const [logo, setLogo] = useState('https://cdn.library.nyu.edu/images/nyulibraries-logo.svg');
-  const [link, setLink] = useState('http://library.nyu.edu');
-  const [imgClass, setImgClass] = useState('image');
+  const [logo, setLogo] = useState(DEFAULT_LOGO);
+  const [link, setLink] = useState(DEFAULT_LINK);
+  const [imgClass, setImgClass] = useState(DEFAULT_IMG_CLASS);
 
   useEffect(() => {
     const institution = getQueryParameter('institution');
