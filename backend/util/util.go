15c9e1d527db4038b468f2f30e12873a450767b2
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-01-27 15:08:13 -0500

    backend/util/util.go: refactored, environment, recovery func added

--- backend/util/util.go
+++ backend/util/util.go
@@ -1,11 +1,12 @@
 package util
 
 import (
-	"github.com/getsentry/sentry-go"
 	"log"
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/getsentry/sentry-go"
 )
 
 func Diff(path1 string, path2 string) (string, error) {
@@ -26,26 +27,47 @@ func Diff(path1 string, path2 string) (string, error) {
 	return string(outputBytes), nil
 }
 
+var validEnvironments = map[string]bool{
+	"dev":  true,
+	"prod": true,
+}
+
+// InitSentry initializes the Sentry SDK and sets up error reporting.
+// It uses the SENTRY_DSN and SENTRY_ENVIRONMENT environment variables to configure the SDK.
 func InitSentry() {
-	// Get the value of the SENTRY_DSN environment variable
+	// Get the values of the SENTRY_DSN and SENTRY_ENVIRONMENT environment variables
 	dsn := os.Getenv("SENTRY_DSN")
-	if dsn == "" {
-		// SENTRY_DSN environment variable is not set
-		log.Println("SENTRY_DSN environment variable is not set")
+	environment := os.Getenv("SENTRY_ENVIRONMENT")
+
+	if dsn == "" || environment == "" {
+		// SENTRY_DSN or SENTRY_ENVIRONMENT environment variable is not set
+		log.Println("Both SENTRY_DSN and SENTRY_ENVIRONMENT environment variables must be set with valid values for Sentry to be initialized")
+	} else if !validEnvironments[environment] {
+		log.Printf("Invalid value for SENTRY_ENVIRONMENT: %s\n", environment)
 	} else {
-		// SENTRY_DSN environment variable is set
-		log.Println("SENTRY_DSN environment variable is set")
 		// Initialize the Sentry Go SDK and set up error reporting
 		err := sentry.Init(sentry.ClientOptions{
-			Dsn:   dsn,
-			Debug: true, // Enable debugging mode
+			Dsn:              dsn,
+			Environment:      environment,
+			Debug:            true, // Enable debugging mode
+			AttachStacktrace: true,
 		})
 		if err != nil {
 			// Failed to initialize the Sentry SDK
-			log.Fatalf("sentry.Init: %s", err)
+			log.Printf("sentry.Init: %s", err)
 		}
 		// Flush buffered events before the program terminates
 		defer sentry.Flush(2 * time.Second)
 	}
-	sentry.CaptureMessage("It works!")
+}
+
+// RecoverWithSentry is a utility function that recovers from panics and sends the panic information to Sentry.
+// It should be called in a defer statement in your main function or in the top-level function of a goroutine.
+// https://docs.sentry.io/platforms/go/panics/
+func RecoverWithSentry() {
+	if r := recover(); r != nil {
+		sentry.CurrentHub().Recover(r)
+		// Optionally, re-panic after sending the panic information to Sentry
+		panic(r)
+	}
 }
