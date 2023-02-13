8f2205124ad81dfd22308f79a7ee67a85ac3a3ca
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-02-13 16:27:45 -0500

    frontend/src/components/Banner/Banner.js: component optimized by removing hooks

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,23 +1,12 @@
-import { DEFAULT_IMG_CLASS, DEFAULT_LINK, DEFAULT_LOGO } from '../../aux/institutionConstants';
 import { getInstitution, getInstitutionQueryParameter } from '../../aux/helpers';
-import { useEffect, useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Banner = () => {
-  const [logo, setLogo] = useState(DEFAULT_LOGO);
-  const [link, setLink] = useState(DEFAULT_LINK);
-  const [imgClass, setImgClass] = useState(DEFAULT_IMG_CLASS);
-
-  useEffect(() => {
-    const institution = getInstitutionQueryParameter('institution');
-    const { logo, link, imgClass } = getInstitution(institution);
-    setLogo(logo);
-    setLink(link);
-    setImgClass(imgClass);
-  }, []);
+  const institution = getInstitutionQueryParameter('institution');
+  const { logo, link, imgClass } = getInstitution(institution);
   return (
     <Navbar className="color-nav" expand="lg">
       <Container>
