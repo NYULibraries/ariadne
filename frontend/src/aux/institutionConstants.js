8d5a43ccae29c65b23108704e8fbe09f78cc35dc
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-02-13 16:24:20 -0500

    frontend/src/aux/institutionConstants.js: refactored using Object.freeze

--- frontend/src/aux/institutionConstants.js
+++ frontend/src/aux/institutionConstants.js
@@ -1,14 +1,20 @@
-export const DEFAULT_IMG_CLASS = 'image';
-export const DEFAULT_LINK = 'http://library.nyu.edu';
-export const DEFAULT_LOGO = 'https://cdn.library.nyu.edu/images/nyulibraries-logo.svg';
+const institutions = Object.freeze({
+    nyu: {
+        logo: 'https://cdn.library.nyu.edu/images/nyulibraries-logo.svg',
+        link: 'http://library.nyu.edu',
+        imgClass: 'image'
+    },
+    nyuad: {
+        logo: `${process.env.PUBLIC_URL}/images/abudhabi-logo-color.svg`,
+        link: 'https://nyuad.nyu.edu/en/library.html',
+        imgClass: 'image white-bg'
+    },
+    nyush: {
+        logo: `${process.env.PUBLIC_URL}/images/shanghai-logo-color.svg`,
+        link: 'https://shanghai.nyu.edu/academics/library',
+        imgClass: 'image white-bg'
+    }
+});
 
-export const NYUAD = 'NYUAD';
-export const NYUAD_IMG_CLASS = 'image white-bg';
-export const NYUAD_LINK = 'https://nyuad.nyu.edu/en/library.html';
-export const NYUAD_LOGO = `${process.env.PUBLIC_URL}/images/abudhabi-logo-color.svg`;
-
-export const NYUSH = 'NYUSH';
-export const NYUSH_IMG_CLASS = 'image white-bg';
-export const NYUSH_LINK = 'https://shanghai.nyu.edu/academics/library';
-export const NYUSH_LOGO = `${process.env.PUBLIC_URL}/images/shanghai-logo-color.svg`;
+export { institutions };
 
