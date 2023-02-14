e330c6d6701daf888c396ff33dabb73771183b44
David <da70@nyu.edu>
2023-02-14 16:18:53 -0500

    e2e/: add .prettierrc (frontend/ rules) and reformat ariadne.spec.js

--- e2e/tests/ariadne.spec.js
+++ e2e/tests/ariadne.spec.js
@@ -2,60 +2,58 @@ const { test, expect } = require('@playwright/test');
 const fs = require('fs');
 const path = require('path');
 
-import {
-  getTestCasesBackendSuccess,
-} from '../../frontend/src/testutils';
+import { getTestCasesBackendSuccess } from '../../frontend/src/testutils';
 
 const testCasesBackendSuccess = getTestCasesBackendSuccess();
 
 for (let i = 0; i < testCasesBackendSuccess.length; i++) {
-  const testCase = testCasesBackendSuccess[ i ];
+  const testCase = testCasesBackendSuccess[i];
   const stubbedBackendAPIResponse = JSON.stringify(testCase.response, null, '    ');
 
   test.describe(`${testCase.name}`, () => {
-      test('HTML matches expected', async ({ page }) => {
-          //Define a mock HTTP request handler for the /v0/ URL path to intercept the request and return a mocked response.
-          await page.route('**/v0/*', async route => {
-              //Return a mock response with a JSON body and a 200 status code
-              await route.fulfill({
-                                      status: 200,
-                                      contentType: 'application/json',
-                                      body: stubbedBackendAPIResponse,
-                                  });
-          });
-
-          await page.goto(`/${testCase.queryString}`);
-          await page.waitForSelector('h6');
-
-          const snapshot = await page.innerHTML('body');
-          // fs.writeFileSync('tests/actual/new-yorker.html', snapshot);
-          const golden = fs.readFileSync(`tests/golden/${testCase.key}.html`, { encoding: 'utf8' });
-          const ok = ( snapshot === golden );
-
-          // Actual diffing is not working in CI:
-
-          // if (!ok) {
-          //   try {
-          //     // eslint-disable-next-line no-unused-vars
-          //     execSync('diff -c tests/golden/new-yorker.html tests/actual/new-yorker.html');
-          //   } catch (error) {
-          // TODO: save stdout and stderr into variables, and print them out in the test assertions
-          //     // eslint-disable-next-line no-console
-          //     console.log(error.stdout.toString());
-          //     // eslint-disable-next-line no-console
-          //     console.log(error.stderr.toString());
-          //   }
-          // }
-          expect(ok).toBe(true);
+    test('HTML matches expected', async ({ page }) => {
+      //Define a mock HTTP request handler for the /v0/ URL path to intercept the request and return a mocked response.
+      await page.route('**/v0/*', async (route) => {
+        //Return a mock response with a JSON body and a 200 status code
+        await route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: stubbedBackendAPIResponse,
+        });
       });
 
-      test('Ask a Librarian link pops up a new Ask a Library tab', async ({ page }) => {
-      await page.route('**/v0/*', async route => {
+      await page.goto(`/${testCase.queryString}`);
+      await page.waitForSelector('h6');
+
+      const snapshot = await page.innerHTML('body');
+      // fs.writeFileSync('tests/actual/new-yorker.html', snapshot);
+      const golden = fs.readFileSync(`tests/golden/${testCase.key}.html`, { encoding: 'utf8' });
+      const ok = snapshot === golden;
+
+      // Actual diffing is not working in CI:
+
+      // if (!ok) {
+      //   try {
+      //     // eslint-disable-next-line no-unused-vars
+      //     execSync('diff -c tests/golden/new-yorker.html tests/actual/new-yorker.html');
+      //   } catch (error) {
+      // TODO: save stdout and stderr into variables, and print them out in the test assertions
+      //     // eslint-disable-next-line no-console
+      //     console.log(error.stdout.toString());
+      //     // eslint-disable-next-line no-console
+      //     console.log(error.stderr.toString());
+      //   }
+      // }
+      expect(ok).toBe(true);
+    });
+
+    test('Ask a Librarian link pops up a new Ask a Library tab', async ({ page }) => {
+      await page.route('**/v0/*', async (route) => {
         await route.fulfill({
-                              status: 200,
-                              contentType: 'application/json',
-                              body: stubbedBackendAPIResponse
-                            });
+          status: 200,
+          contentType: 'application/json',
+          body: stubbedBackendAPIResponse,
+        });
       });
 
       await page.goto(`/${testCase.queryString}`);
@@ -73,13 +71,13 @@ for (let i = 0; i < testCasesBackendSuccess.length; i++) {
 
     test('matches screenshot', async ({ page }) => {
       //Define a mock HTTP request handler for the /v0/ URL path to intercept the request and return a mocked response.
-      await page.route('**/v0/*', async route => {
+      await page.route('**/v0/*', async (route) => {
         //Return a mock response with a JSON body and a 200 status code
         await route.fulfill({
-                              status: 200,
-                              contentType: 'application/json',
-                              body: stubbedBackendAPIResponse
-                            });
+          status: 200,
+          contentType: 'application/json',
+          body: stubbedBackendAPIResponse,
+        });
       });
 
       //Navigate to the frontend URL, which will make a request to the backend URL
@@ -94,32 +92,29 @@ for (let i = 0; i < testCasesBackendSuccess.length; i++) {
     });
 
     test('renders links with a list-group className', async ({ page }) => {
-      await page.route('**/v0/*', route => {
-                         route.fulfill({
-                                         status: 200,
-                                         contentType: 'application/json',
-                                         body: stubbedBackendAPIResponse
-                                       });
-                       }
-      );
+      await page.route('**/v0/*', (route) => {
+        route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: stubbedBackendAPIResponse,
+        });
+      });
 
       await page.goto(`/${testCase.queryString}`);
       expect(await page.$('.list-group')).toBeTruthy();
     });
 
     test('renders the search results', async ({ page }) => {
-      await page.route('**/v0/*', route => {
+      await page.route('**/v0/*', (route) => {
         route.fulfill({
-                        status: 200,
-                        contentType: 'application/json',
-                        body: stubbedBackendAPIResponse
-                      });
+          status: 200,
+          contentType: 'application/json',
+          body: stubbedBackendAPIResponse,
+        });
       });
 
       await page.goto(`/${testCase.queryString}`);
       expect(await page.textContent('p')).toBe('Displaying search results...');
     });
-
   });
 }
-
