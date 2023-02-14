b220d05726976d638c38108b5ba68e1286bea25e
David <da70@nyu.edu>
2023-02-14 16:24:07 -0500

    e2e/: rename stubbedBackendAPIResponse -> stubbedBackendAPIResponseBody

--- e2e/tests/ariadne.spec.js
+++ e2e/tests/ariadne.spec.js
@@ -7,20 +7,22 @@ const testCasesBackendSuccess = getTestCasesBackendSuccess();
 
 for (let i = 0; i < testCasesBackendSuccess.length; i++) {
   const testCase = testCasesBackendSuccess[i];
-  const stubbedBackendAPIResponse = JSON.stringify(testCase.response, null, '    ');
+  const stubbedBackendAPIResponseBody = JSON.stringify(testCase.response, null, '    ');
 
   test.describe(`${testCase.name}`, () => {
-    test('HTML matches expected', async ({ page }) => {
+    test.beforeEach(async() => {
       //Define a mock HTTP request handler for the /v0/ URL path to intercept the request and return a mocked response.
       await page.route('**/v0/*', async (route) => {
         //Return a mock response with a JSON body and a 200 status code
         await route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: stubbedBackendAPIResponse,
-        });
+                              status: 200,
+                              contentType: 'application/json',
+                              body: stubbedBackendAPIResponseBody,
+                            });
       });
+    });
 
+    test('HTML matches expected', async ({ page }) => {
       await page.goto(`/${testCase.queryString}`);
       await page.waitForSelector('h6');
 
@@ -47,14 +49,6 @@ for (let i = 0; i < testCasesBackendSuccess.length; i++) {
     });
 
     test('Ask a Librarian link pops up a new Ask a Library tab', async ({ page }) => {
-      await page.route('**/v0/*', async (route) => {
-        await route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: stubbedBackendAPIResponse,
-        });
-      });
-
       await page.goto(`/${testCase.queryString}`);
       // Playwright's team recommendation for handling popups: https://playwright.dev/docs/pages#handling-popups
       // Start waiting for popup before clicking. Note no await.
@@ -69,16 +63,6 @@ for (let i = 0; i < testCasesBackendSuccess.length; i++) {
     });
 
     test('matches screenshot', async ({ page }) => {
-      //Define a mock HTTP request handler for the /v0/ URL path to intercept the request and return a mocked response.
-      await page.route('**/v0/*', async (route) => {
-        //Return a mock response with a JSON body and a 200 status code
-        await route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: stubbedBackendAPIResponse,
-        });
-      });
-
       //Navigate to the frontend URL, which will make a request to the backend URL
       await page.goto(`/${testCase.queryString}`);
 
@@ -91,27 +75,11 @@ for (let i = 0; i < testCasesBackendSuccess.length; i++) {
     });
 
     test('renders links with a list-group className', async ({ page }) => {
-      await page.route('**/v0/*', (route) => {
-        route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: stubbedBackendAPIResponse,
-        });
-      });
-
       await page.goto(`/${testCase.queryString}`);
       expect(await page.$('.list-group')).toBeTruthy();
     });
 
     test('renders the search results', async ({ page }) => {
-      await page.route('**/v0/*', (route) => {
-        route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: stubbedBackendAPIResponse,
-        });
-      });
-
       await page.goto(`/${testCase.queryString}`);
       expect(await page.textContent('p')).toBe('Displaying search results...');
     });
