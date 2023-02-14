895cd10ff866569ddc22d4a52f4e18a41a5b8ec4
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-02-14 13:34:26 -0500

    PUBLIC_URL name updated, set for env.development

--- frontend/src/aux/institutionConstants.js
+++ frontend/src/aux/institutionConstants.js
@@ -1,20 +1,25 @@
-const institutions = Object.freeze({
+const institutions = {
     nyu: {
         logo: 'https://cdn.library.nyu.edu/images/nyulibraries-logo.svg',
         link: 'http://library.nyu.edu',
         imgClass: 'image'
     },
     nyuad: {
-        logo: `${process.env.PUBLIC_URL}/images/abudhabi-logo-color.svg`,
+        logo: `${process.env.REACT_APP_PUBLIC_URL}/images/abudhabi-logo-color.svg`,
         link: 'https://nyuad.nyu.edu/en/library.html',
         imgClass: 'image white-bg'
     },
     nyush: {
-        logo: `${process.env.PUBLIC_URL}/images/shanghai-logo-color.svg`,
+        logo: `${process.env.REACT_APP_PUBLIC_URL}/images/shanghai-logo-color.svg`,
         link: 'https://shanghai.nyu.edu/academics/library',
         imgClass: 'image white-bg'
     }
-});
+};
+
+// eslint-disable-next-line no-console
+console.log(institutions.nyuad.logo)
+// eslint-disable-next-line no-console
+console.log('process.env.PUBLIC_URL:', process.env.REACT_APP_PUBLIC_URL);
 
 export { institutions };
 
