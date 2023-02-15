e5ff8883e7c070f8365dbe999285536b847d5af5
David <da70@nyu.edu>
2023-02-15 15:56:21 -0500

    e2e/testutils/index.js: add comment explaining why we can't use a custom flag like  with playwright

--- e2e/testutils/index.js
+++ e2e/testutils/index.js
@@ -4,6 +4,10 @@ function removeSourceMappingUrlComments(html) {
   return html.replace( regex, '/* E2E TEST EDIT: sourceMappingURL comments elided */' );
 }
 
+// NOTE: it's current not possible to use a custom flag like `--update-golden-files`
+// with `playwright`:
+// "[Feature] Add support for test.each / describe.each #7036"
+// https://github.com/microsoft/playwright/issues/7036
 function updateGoldenFiles() {
   return process.env.UPDATE_GOLDEN_FILES &&
          process.env.UPDATE_GOLDEN_FILES.toLowerCase() !== 'false';
