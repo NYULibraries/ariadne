ff98abdcdd2d719b53c8c7ba95500b1a326ccb63
Eric Griffis <eric.griffis@nyu.edu>
2023-02-16 11:55:34 -0500

    Frontend - remove domain name from from institution logos

--- frontend/src/aux/institutionConstants.js
+++ frontend/src/aux/institutionConstants.js
@@ -5,12 +5,12 @@ const institutions = Object.freeze({
         imgClass: 'image'
     },
     nyuad: {
-        logo: `${process.env.REACT_APP_PUBLIC_URL}/images/abudhabi-logo-color.svg`,
+        logo: `/images/abudhabi-logo-color.svg`,
         link: 'https://nyuad.nyu.edu/en/library.html',
         imgClass: 'image white-bg'
     },
     nyush: {
-        logo: `${process.env.REACT_APP_PUBLIC_URL}/images/shanghai-logo-color.svg`,
+        logo: `/images/shanghai-logo-color.svg`,
         link: 'https://shanghai.nyu.edu/academics/library',
         imgClass: 'image white-bg'
     }
