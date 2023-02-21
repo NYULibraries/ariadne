cadeccd0892b5665741c7c5168c650a7ee0acfec
David <da70@nyu.edu>
2023-02-21 12:10:00 -0500

    frontend/src/aux/helpers.js: alphasort function definitions

--- frontend/src/aux/helpers.js
+++ frontend/src/aux/helpers.js
@@ -1,11 +1,16 @@
-//  Helper functions for useApi hook
-function getLinks(jsonData) {
-  return jsonData.ctx_obj[0].ctx_obj_targets[0].target;
-}
-
 // Helper function for List.js component
 function getCoverageStatement(link) {
   return link.coverage[0].coverage_text[0].threshold_text[0].coverage_statement?.join('. ');
+}
+
+function getInstitutionQueryParameter(parameterName) {
+  const queryString = window.location.search;
+  return getParameterFromQueryString(queryString, parameterName);
+}
+
+//  Helper functions for useApi hook
+function getLinks(jsonData) {
+  return jsonData.ctx_obj[0].ctx_obj_targets[0].target;
 }
 
 // Helper functions for Banner.js component
@@ -14,15 +19,10 @@ function getParameterFromQueryString(queryString, parameterName) {
   return urlParams.get(parameterName);
 }
 
-function getInstitutionQueryParameter(parameterName) {
-  const queryString = window.location.search;
-  return getParameterFromQueryString(queryString, parameterName);
-}
-
 export {
   getCoverageStatement,
+  getInstitutionQueryParameter,
   getLinks,
   getParameterFromQueryString,
-  getInstitutionQueryParameter
 };
 
