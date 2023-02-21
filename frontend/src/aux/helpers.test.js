508ad552c24ff969a932692cf24f0797f0d53cd7
David <da70@nyu.edu>
2023-02-21 12:48:55 -0500

    frontend/src/aux/helpers.test.js: cover all case combinations for query param name and value

--- frontend/src/aux/helpers.test.js
+++ frontend/src/aux/helpers.test.js
@@ -12,12 +12,28 @@ describe.each(institutionNamesUpperCase)(
           expect(returnedValue).toBe(expectedValue);
       });
 
+      test(`returns the correct value for "institution=${institutionName.toLowerCase()}" query parameter`, () => {
+        const queryString = `?institution=${institutionName.toLowerCase()}`;
+        const parameterName = 'institution';
+        const expectedValue = institutionName.toLowerCase();
+        const returnedValue = getParameterFromQueryString(queryString, parameterName);
+        expect(returnedValue).toBe(expectedValue);
+      });
+
       test(`returns the correct value for "INSTITUTION=${institutionName}" query parameter`, () => {
           const queryString = `?INSTITUTION=${institutionName}`;
           const parameterName = 'INSTITUTION';
           const expectedValue = institutionName.toLowerCase();
           const returnedValue = getParameterFromQueryString(queryString, parameterName);
           expect(returnedValue).toBe(expectedValue);
+      });
+
+      test(`returns the correct value for "INSTITUTION=${institutionName.toLowerCase()}" query parameter`, () => {
+        const queryString = `?INSTITUTION=${institutionName.toLowerCase()}`;
+        const parameterName = 'INSTITUTION';
+        const expectedValue = institutionName.toLowerCase();
+        const returnedValue = getParameterFromQueryString(queryString, parameterName);
+        expect(returnedValue).toBe(expectedValue);
       });
     }
 );
