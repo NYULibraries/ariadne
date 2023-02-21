1b00646d943a681ea9638743d22e5afdbb08a68e
David <da70@nyu.edu>
2023-02-21 12:08:15 -0500

    frontend/src/aux/helpers.js: use `function` to declare functions instead of `const` and arror function expressions

--- frontend/src/aux/helpers.js
+++ frontend/src/aux/helpers.js
@@ -1,26 +1,25 @@
 //  Helper functions for useApi hook
-const getLinks = (jsonData) => {
+function getLinks(jsonData) {
   return jsonData.ctx_obj[0].ctx_obj_targets[0].target;
-};
+}
 
 // Helper function for List.js component
-const getCoverageStatement = (link) => {
+function getCoverageStatement(link) {
   return link.coverage[0].coverage_text[0].threshold_text[0].coverage_statement?.join('. ');
-};
+}
 
 // Helper functions for Banner.js component
-const getParameterFromQueryString = (queryString, parameterName) => {
+function getParameterFromQueryString(queryString, parameterName) {
   const urlParams = new URLSearchParams(queryString.toLowerCase());
   let parameter = urlParams.get(parameterName);
 
   return parameter;
-};
+}
 
-
-const getInstitutionQueryParameter = (parameterName) => {
+function getInstitutionQueryParameter(parameterName) {
   const queryString = window.location.search;
   return getParameterFromQueryString(queryString, parameterName);
-};
+}
 
 export {
   getCoverageStatement,
