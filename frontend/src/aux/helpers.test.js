22092c7c0b6e1a67fcec7627c1018088e3741082
David <da70@nyu.edu>
2023-02-21 12:40:54 -0500

    frontend/src/aux/helpers.test.js: parameterize tests in `describe.each` loop

--- frontend/src/aux/helpers.test.js
+++ frontend/src/aux/helpers.test.js
@@ -1,28 +1,14 @@
 import { getParameterFromQueryString } from './helpers';
 
-describe('getParameterFromQueryString', () => {
-    test('returns the correct value for "institution=NYU" query parameter', () => {
-        const queryString = '?institution=NYU';
-        const parameterName = 'institution';
-        const expectedValue = 'nyu';
-        const returnedValue = getParameterFromQueryString(queryString, parameterName);
-        expect(returnedValue).toBe(expectedValue);
-    });
+describe.each(['NYU', 'NYUAD', 'NYUSH'])(
+    'Institution name: %s', (institutionName) => {
+      test(`returns the correct value for "institution=${institutionName}" query parameter`, () => {
+          const queryString = `?institution=${institutionName}`;
+          const parameterName = 'institution';
+          const expectedValue = institutionName.toLowerCase();
+          const returnedValue = getParameterFromQueryString(queryString, parameterName);
+          expect(returnedValue).toBe(expectedValue);
+      });
+    }
+);
 
-    test('returns the correct value for "institution=NYUAD" query parameter', () => {
-        const queryString = '?institution=NYUAD';
-        const parameterName = 'institution';
-        const expectedValue = 'nyuad';
-        const returnedValue = getParameterFromQueryString(queryString, parameterName);
-        expect(returnedValue).toBe(expectedValue);
-    });
-
-    test('returns the correct value for "institution=NYUSH" query parameter', () => {
-        const queryString = '?institution=NYUSH';
-        const parameterName = 'institution';
-        const expectedValue = 'nyush';
-        const returnedValue = getParameterFromQueryString(queryString, parameterName);
-        expect(returnedValue).toBe(expectedValue);
-    });
-
-});
