bda76eeb8f5ea2b94b2607f1f4837c73c6fe4368
David <da70@nyu.edu>
2023-02-21 13:06:30 -0500

    Banner.test.js: parameterize tests in `describe.each` loop and add coverage for institution=nyu image class

--- frontend/src/components/Banner/Banner.test.js
+++ frontend/src/components/Banner/Banner.test.js
@@ -2,44 +2,49 @@ import { render, screen, waitFor } from '@testing-library/react';
 
 import Banner from './Banner';
 
-test('renders the Banner component', () => {
-  render(<Banner />);
-});
+import { bannerInstitutionInfo } from '../../aux/institutionConstants';
 
-test('renders the NYU Libraries logo', async () => {
-  render(<Banner />);
-  const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i));
-  expect(linkElement).toBeInTheDocument();
-});
+const institutionNamesUpperCase = Object.keys(bannerInstitutionInfo).map(institutionName => institutionName.toUpperCase());
 
-test('renders correctly', () => {
-  const { asFragment } = render(<Banner />);
-  expect(asFragment()).toMatchSnapshot();
-});
+describe.each(institutionNamesUpperCase)(
+  'Institution name: %s', (institutionNameUpperCase) => {
+    beforeEach( () => {
+      window.history.pushState({}, null, `/?institution=${institutionNameUpperCase}`);
+    });
 
-test('renders the correct NYUAD logo and link based on institution query parameter', async () => {
-  window.history.pushState({}, null, '/?institution=NYUAD');
-  render(<Banner />);
-  const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i).closest('a'));
-  expect(linkElement).toHaveAttribute('href', 'https://nyuad.nyu.edu/en/library.html');
-  const imgElement = linkElement.querySelector('img');
-  expect(imgElement).toHaveAttribute('src', `/images/abudhabi-logo-color.svg`);
-});
+    test(`renders ${institutionNameUpperCase} page correctly`, () => {
+      const { asFragment } = render(<Banner />);
+      expect(asFragment()).toMatchSnapshot();
+    });
 
-test('renders the correct NYUSH logo and link based on institution query parameter', async () => {
-  window.history.pushState({}, null, '/?institution=NYUSH');
-  render(<Banner />);
-  const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i).closest('a'));
-  expect(linkElement).toHaveAttribute('href', 'https://shanghai.nyu.edu/academics/library');
-  const imgElement = linkElement.querySelector('img');
-  expect(imgElement).toHaveAttribute('src', `/images/shanghai-logo-color.svg`);
-});
+    test(`renders the correct ${institutionNameUpperCase} logo and link based on institution query parameter`, async () => {
+      render(<Banner />);
+      const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i).closest('a'));
+      const { logo, link } = bannerInstitutionInfo[institutionNameUpperCase.toLowerCase()]
+      expect(linkElement).toHaveAttribute('href', link);
+      const imgElement = linkElement.querySelector('img');
+      expect(imgElement).toHaveAttribute('src', logo);
+    });
 
-test('changes the background of the logo correctly when institution is NYUSH or NYUAD', async () => {
-  const institution = 'NYUAD';
-  render(<Banner />, {
-    route: `?institution=${institution}`,
+    test(`sets the background of the logo correctly for ${institutionNameUpperCase}`, async () => {
+      render(<Banner />, {
+                           route: `?institution=${institutionNameUpperCase}`,
+                         });
+      const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i));
+      const { imgClass } = bannerInstitutionInfo[institutionNameUpperCase.toLowerCase()]
+      expect(linkElement).toHaveClass(imgClass);
+    });
+  }
+)
+
+describe('No `institution` parameter in query string', () => {
+  test('renders the Banner component', () => {
+    render(<Banner />);
   });
-  const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i));
-  expect(linkElement).toHaveClass('image white-bg');
+
+  test('renders the NYU Libraries logo', async () => {
+    render(<Banner />);
+    const linkElement = await waitFor(() => screen.getByAltText(/NYU Libraries logo/i));
+    expect(linkElement).toBeInTheDocument();
+  });
 });
