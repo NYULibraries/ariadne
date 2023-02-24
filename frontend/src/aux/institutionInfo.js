3d601a2fc7da4d907a71b701048630c09433f9bc
Vlad Burlutskiy <vlad.burlutskiy@nyu.edu>
2023-02-24 13:08:32 -0500

    frontend/src/aux/institutionInfo.js: altLibraryLogoImageText key added to institutions

--- frontend/src/aux/institutionInfo.js
+++ frontend/src/aux/institutionInfo.js
@@ -4,17 +4,20 @@ const bannerInstitutionInfo = deepFreeze({
     nyu: {
         logo: 'https://cdn.library.nyu.edu/images/nyulibraries-logo.svg',
         link: 'http://library.nyu.edu',
-        imgClass: 'image'
+        imgClass: 'image',
+        altLibraryLogoImageText: 'NYU Libraries logo - click to go to the NYU Libraries main page'
     },
     nyuad: {
         logo: `/images/abudhabi-logo-color.svg`,
         link: 'https://nyuad.nyu.edu/en/library.html',
-        imgClass: 'image white-bg'
+        imgClass: 'image white-bg',
+        altLibraryLogoImageText: 'NYU Abu Dhabi Library logo - click to go to the NYU Abu Dhabi Library main page'
     },
     nyush: {
         logo: `/images/shanghai-logo-color.svg`,
         link: 'https://shanghai.nyu.edu/academics/library',
-        imgClass: 'image white-bg'
+        imgClass: 'image white-bg',
+        altLibraryLogoImageText: 'NYU Shanghai Library logo - click to go to the NYU Shanghai Library main page'
     }
 });
 
