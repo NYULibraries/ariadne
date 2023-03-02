d4d71bb5c8473efb5c5126bf5d9559f7ab4b4807
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-02 17:47:53 -0500

    frontend/src/components/List/List.js: Update bootstrap grid

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -42,21 +42,29 @@ const List = () => {
   return (
     <main>
       <>
-        <div className="jumbotron">
-          <div className="container text-center">
-            <h1>{RESULTS_HEADER_TEXT}</h1>
-            <p>Note: Alternate titles might have matched your search terms</p>
-          </div>
-        </div>
         {/* TODO: we could put a spinner here: */}
-        {backendClient.loading && <div className="loader" aria-label="Loading...">{LOADING_TEXT}</div>}
-        {backendClient.error && <div className="i-am-centered">{backendClient.error}</div>}
         <Container>
           <Row md={12}>
             <Col md={8}>
+              <div className="jumbotron">
+                <div className="container text-left">
+                  <h1 style={{ "fontWeight": "bold" }}>{RESULTS_HEADER_TEXT}</h1>
+                  <div>
+                    <Citation />
+                  </div>
+                </div>
+              </div>
+              {/* </Col>
+            <Col md={8}> */}
+              {backendClient.loading && <div className="loading-container">
+                <div aria-label="Loading...">{LOADING_TEXT}</div>
+              </div>}
+              {backendClient.error && <div>{backendClient.error}</div>}
+              {/* </Col>
+            <Col md={8}> */}
               <div className="list-group">
                 <div className="list-group-item list-group-item-action flex-column border-0">
-                  <Citation />
+                  {/* <Citation /> */}
                 </div>
                 {backendClient.resource?.map((link, idx) => (
                   <div key={idx} className="list-group-item list-group-item-action flex-column border-0">
@@ -89,6 +97,18 @@ const List = () => {
                         {ASK_LIBRARIAN_TEXT}
                       </a>
                     </h5>
+                    <md-card-content>
+                      <p>Use <a href="https://library.nyu.edu/ask/" target="_blank" className="md-primoExplore-theme" rel="noreferrer">Ask A Librarian</a> or the &quot;Chat with Us&quot; icon at the bottom right corner for any question you have about the Libraries&apos; services.</p>
+                      <p>Visit our <a href="https://guides.nyu.edu/online-tutorials/finding-sources" target="_blank" className="md-primoExplore-theme" rel="noreferrer">online tutorials</a> for tips on searching the catalog and getting library resources.</p>
+                      <h3 className="md-subhead">Additional Resources</h3>
+                      <ul>
+                        <li>Use <a href="https://ezborrow.reshare.indexdata.com/" target="_blank" className="md-primoExplore-theme" rel="noreferrer">EZBorrow</a> or <a href="https://library.nyu.edu/services/borrowing/from-non-nyu-libraries/interlibrary-loan/" target="_blank" className="md-primoExplore-theme" rel="noreferrer">InterLibrary Loan (ILL)</a> for materials unavailable at NYU</li>
+                        <li>Discover subject specific resources using <a href="http://guides.nyu.edu" target="_blank" className="md-primoExplore-theme" rel="noreferrer">expert curated research guides</a></li>
+                        <li>Explore the <a href="https://library.nyu.edu/services/" target="_blank" className="md-primoExplore-theme" rel="noreferrer">complete list of library services</a></li>
+                        <li>Reach out to the Libraries on <a href="https://www.instagram.com/nyulibraries/" target="_blank" className="md-primoExplore-theme" rel="noreferrer">our Instagram</a></li>
+                        <li>Search <a href="https://www.worldcat.org/search?qt=worldcat_org_all" target="_blank" className="md-primoExplore-theme" rel="noreferrer">WorldCat</a> for items in nearby libraries</li>
+                      </ul>
+                    </md-card-content>
                   </div>
                 )}
               </aside>
@@ -103,6 +123,6 @@ const List = () => {
 export const ASK_LIBRARIAN_TEXT = 'Ask a Librarian';
 export const ASK_LIBRARIAN_URL = 'https://library.nyu.edu/ask/';
 export const LOADING_TEXT = 'Loading...';
-export const RESULTS_HEADER_TEXT = 'Displaying search results...';
+export const RESULTS_HEADER_TEXT = 'Getit Search Results:';
 
 export default List;
