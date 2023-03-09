814454887db9ea885fecfbe130a95c5d67af0901
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-08 19:03:22 -0500

    frontend/src/App.test.js: Update integration tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,9 @@
 import { cleanup, render } from '@testing-library/react';
+
 import App from './App';
-import List from './components/List/List';
 import Banner from './components/Banner/Banner';
+import PageFooter from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 afterEach(cleanup);
 
@@ -10,9 +12,14 @@ test('renders the App component', () => {
 });
 
 test('renders the List component', () => {
-  render(<List />);
+  render(<Main />);
 });
 
 test('renders the Banner component', () => {
   render(<Banner />);
 });
+
+test('renders the PageFooter component', () => {
+  render(<PageFooter />);
+});
+
