513a7ffff41178e7aafc6b8e29a9fe460886a74b
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-10 12:41:12 -0500

    frontend/src/components/AskLibrarian/: Update integration tests and snapshots

--- frontend/src/components/AskLibrarian/AskLibrarian.test.js
+++ frontend/src/components/AskLibrarian/AskLibrarian.test.js
@@ -6,34 +6,25 @@ import React from 'react';
 
 describe('AskLibrarian component', () => {
     it('renders "Need Help?" when not loading', () => {
-        const { getByText } = render(<AskLibrarian loading={false} />);
+        const { getByText } = render(<AskLibrarian />);
         expect(getByText('Need Help?')).toBeInTheDocument();
     });
 
     it('renders the correct ASK_LIBRARIAN_TEXT and ASK_LIBRARIAN_URL', () => {
-        const { getByText } = render(<AskLibrarian loading={false} />);
+        const { getByText } = render(<AskLibrarian />);
         const link = getByText(ASK_LIBRARIAN_TEXT);
         expect(link).toBeInTheDocument();
         expect(link.getAttribute('href')).toBe(ASK_LIBRARIAN_URL);
     });
 
     it('renders additional resources when not loading', () => {
-        const { queryAllByText } = render(<AskLibrarian loading={false} />);
+        const { queryAllByText } = render(<AskLibrarian />);
         expect(queryAllByText(/Additional Resources/).length).toBeGreaterThan(0);
-        // expect(queryAllByText(/Visit our online tutorials for tips on searching the catalog and getting library resources/).length).toBeGreaterThan(0);
-        // expect(queryAllByText('Discover subject specific resources using expert curated research guides').length).toBeGreaterThan(0);
-        // expect(queryAllByText('Explore the complete list of library services').length).toBeGreaterThan(0);
-        // expect(queryAllByText('Reach out to the Libraries on our Instagram').length).toBeGreaterThan(0);
-        // expect(queryAllByText('Search WorldCat for items in nearby libraries').length).toBeGreaterThan(0);
     });
 
-    it('does not render anything when loading', () => {
-        const { container } = render(<AskLibrarian loading={true} />);
-        expect(container.firstChild).toBeNull();
-    });
 
     it('opens the ASK_A_LIBRARIAN_URL in a new tab when the link is clicked', () => {
-        const { getByText } = render(<AskLibrarian loading={false} />);
+        const { getByText } = render(<AskLibrarian />);
         const link = getByText(ASK_LIBRARIAN_TEXT);
         userEvent.click(link);
         expect(link).toHaveAttribute('target', '_blank');
@@ -41,13 +32,8 @@ describe('AskLibrarian component', () => {
         expect(link).toHaveAttribute('href', ASK_LIBRARIAN_URL);
     });
 
-    it('matches snapshot when not loading', () => {
-        const { container } = render(<AskLibrarian loading={false} />);
-        expect(container.firstChild).toMatchSnapshot();
-    });
-
-    it('matches snapshot when loading', () => {
-        const { container } = render(<AskLibrarian loading={true} />);
+    it('matches snapshot', () => {
+        const { container } = render(<AskLibrarian />);
         expect(container.firstChild).toMatchSnapshot();
     });
 });
