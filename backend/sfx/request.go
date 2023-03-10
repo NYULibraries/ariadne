f1f15a24c6c0ef4b2bdfabf8f249c1e10b5176e4
David <da70@nyu.edu>
2023-03-10 18:50:00 -0500

    backend/sfx/: remove any code that's not being used after switch from POST to GET, and make minimal adjustment to unit test to keep it from throwing errors

--- backend/sfx/request.go
+++ backend/sfx/request.go
@@ -1,44 +1,16 @@
 package sfx
 
 import (
-	"bytes"
 	_ "embed"
-	"encoding/xml"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"reflect"
-	"regexp"
-	"strings"
-	"text/template"
-	"time"
 )
 
-//go:embed templates/context-objects.xml
-var sfxRequestTemplate string
-
-// Source: https://learning.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch09s05.html#markup-xmlname-discussion
-// "XML 1.0 names (approximate)"
-// We use the version 1.0 regular expression because the SFX API request is in XML 1.0:
-// https://developers.exlibrisgroup.com/sfx/apis/web_services/openurl/
-// Note that there are edge case strings that could in theory slip through, but
-// for our purposes it wouldn't matter because such strings would not be valid
-// SFX request element names.
-var validQueryParamNameRegexp = regexp.MustCompile("^[:_\\p{Ll}\\p{Lu}\\p{Lt}\\p{Lo}\\p{Nl}][:_\\-.\\p{L}\\p{M}\\p{Nd}\\p{Nl}]*$")
-
-// Object representing everything that's needed to request from SFX
 type SFXRequest struct {
 	DumpedHTTPRequest string
 	HTTPRequest       http.Request
-	RequestXML        string
-}
-
-// Values needed for templating an SFX request are parsed
-type sfxRequestBodyParams struct {
-	RftValues *map[string][]string
-	Timestamp string
-	Genre     string
 }
 
 func (c SFXRequest) do() (*SFXResponse, error) {
@@ -60,19 +32,9 @@ func (c SFXRequest) do() (*SFXResponse, error) {
 func NewSFXRequest(queryStringValues url.Values) (*SFXRequest, error) {
 	sfxRequest := &SFXRequest{}
 
-	requestBodyParams, err := parseRequestParams(queryStringValues)
+	httpRequest, err := newSFXHTTPRequest(queryStringValues)
 	if err != nil {
-		return sfxRequest, fmt.Errorf("could not parse required request body params from querystring: %v", err)
-	}
-
-	sfxRequest.RequestXML, err = requestXML(requestBodyParams)
-	if err != nil {
-		return sfxRequest, fmt.Errorf("could not convert multiple objects request to XML: %v", err)
-	}
-
-	httpRequest, err := newSFXHTTPRequest(sfxRequest.RequestXML, queryStringValues)
-	if err != nil {
-		return sfxRequest, fmt.Errorf("could not create new multiple objects request: %v", err)
+		return sfxRequest, fmt.Errorf("could not create new SFX request: %v", err)
 	}
 	// NOTE: This appears to drain httpRequest.Body, so when getting the dumped
 	// HTTP request later, make sure to get it from sfxRequest.HTTPRequest
@@ -87,31 +49,6 @@ func NewSFXRequest(queryStringValues url.Values) (*SFXRequest, error) {
 	sfxRequest.DumpedHTTPRequest = string(dumpedHTTPRequest)
 
 	return sfxRequest, nil
-}
-
-func escapeQueryParamValuesForXML(values []string) ([]string, error) {
-	var escapedValues []string
-	var err error
-
-	for _, value := range values {
-		// For a query string containing this:
-		//
-		//    title=Journal%20of%20the%20Gilded%20Age%20%26%20Progressive%20Era
-		//
-		// ...the construction of the SFX request body will fail due to this element:
-		//
-		//     <rft:title>Journal of the Gilded Age & Progressive Era</rft:title>
-		//
-		// ...which is illegal due to the unescaped ampersand.
-		buf := bytes.NewBuffer(make([]byte, 0))
-		err = xml.EscapeText(buf, []byte(value))
-		if err != nil {
-			return escapedValues, err
-		}
-		escapedValues = append(escapedValues, buf.String())
-	}
-
-	return escapedValues, err
 }
 
 // transforms sid to rfr_id since the former seems to trigger SFX errors when its value contains certain unicode encodings, while the latter doesn't
@@ -129,8 +66,7 @@ func filterOpenURLParams(queryStringValues url.Values) url.Values {
 	return queryStringValues
 }
 
-func newSFXHTTPRequest(requestXML string, queryStringValues url.Values) (*http.Request, error) {
-	//params := url.Values{}
+func newSFXHTTPRequest(queryStringValues url.Values) (*http.Request, error) {
 	params := filterOpenURLParams(queryStringValues)
 	params.Add("url_ctx_fmt", "info:ofi/fmt:xml:xsd:ctx")
 	params.Add("sfx.response_type", "multi_obj_xml")
@@ -140,106 +76,12 @@ func newSFXHTTPRequest(requestXML string, queryStringValues url.Values) (*http.R
 		params.Add("sfx.ignore_date_threshold", "1")
 	}
 	params.Add("sfx.doi_url", "http://dx.doi.org")
-	//params.Add("url_ctx_val", requestXML)
 
-	//request, err := http.NewRequest("GET", sfxURL, strings.NewReader(params.Encode()))
 	queryURL := sfxURL + "?" + params.Encode()
 	request, err := http.NewRequest("GET", queryURL, nil)
 	if err != nil {
 		return request, fmt.Errorf("could not initialize request to SFX server: %v", err)
 	}
 
-	//request.PostForm = params
-	//request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
 	return request, nil
-}
-
-// Parse SFX request body params from querystring.
-func parseRequestParams(queryStringValues url.Values) (sfxRequestBodyParams, error) {
-	params := sfxRequestBodyParams{}
-
-	rfts := &map[string][]string{}
-
-	for queryName, queryValue := range queryStringValues {
-		// Unescaped "&" characters in query param values can split the value
-		// string and cause the substrings to be interpreted as query names.
-		// Example: title=Journal%20of%20the%20Gilded%20Age%20%&%20Progressive%20Era
-		// This would lead to a queryName of " Progressive Era" in this loop, which
-		// would then cause construction of the XLM for teh SFX request body to
-		// fail due to this illegal XML element:
-		//               <rft: progressive era></rft: progressive era>
-		// There may be other edge case query strings which produce bad XML element
-		// names.
-		if !validQueryParamNameRegexp.MatchString(queryName) {
-			continue
-		}
-
-		// Drop if there is also a query param that has the same name but with "rft."-prefix.
-		// We are allow with and without prefix, but if both exist, we drop the
-		// non-prefixed param.
-		if _, ok := queryStringValues[fmt.Sprintf("rft.%s", queryName)]; ok {
-			continue
-		}
-
-		// Deal with encoded ampersands in param values.
-		// Example: title=Journal%20of%20the%20Gilded%20Age%20%26%20Progressive%20Era
-		// If the ampersands are not escaped, the construction of the XML for the
-		// SFX request body will fail.
-		escapedValue, err := escapeQueryParamValuesForXML(queryValue)
-		if err != nil {
-			return params, fmt.Errorf("unable to XML escape value for query string param %s: %v", queryName, err)
-		}
-		// Strip the "rft." prefix from the param name and map to valid OpenURL fields
-		if strings.HasPrefix(queryName, "rft.") {
-			// E.g. "rft.book" becomes "book"
-			newKey := strings.Split(queryName, ".")[1]
-			(*rfts)[newKey] = escapedValue
-			// Without "rft." prefix, use the whole param name
-		} else {
-			(*rfts)[queryName] = escapedValue
-		}
-	}
-
-	if reflect.DeepEqual(rfts, &map[string][]string{}) {
-		return params, fmt.Errorf("no valid querystring values to parse")
-	}
-
-	// "Add support for OpenURLs with no genre parameter"
-	// https://nyu-lib.monday.com/boards/765008773/pulses/4036893558
-	if (*rfts)["genre"] == nil {
-		(*rfts)["genre"] = []string{defaultGenre}
-	}
-
-	genre, err := validGenre((*rfts)["genre"])
-	if err != nil {
-		return params, fmt.Errorf("genre is not valid: %v", err)
-	}
-
-	now := time.Now()
-	params.Timestamp = now.Format(time.RFC3339Nano)
-	params.RftValues = rfts
-	params.Genre = genre
-
-	return params, nil
-}
-
-func requestXML(templateValues sfxRequestBodyParams) (string, error) {
-	t := template.New("sfx-request.xml").Funcs(template.FuncMap{"ToLower": strings.ToLower})
-
-	t, err := t.Parse(sfxRequestTemplate)
-	if err != nil {
-		return "", fmt.Errorf("could not load template parse file: %v", err)
-	}
-
-	var tpl bytes.Buffer
-	if err = t.Execute(&tpl, templateValues); err != nil {
-		return "", fmt.Errorf("could not execute go template from multiple objects request: %v", err)
-	}
-
-	if !isValidXML(tpl.Bytes()) {
-		return "", fmt.Errorf("request multiple objects XML is not valid XML: %v", err)
-	}
-
-	return tpl.String(), nil
 }
--- backend/sfx/request_test.go
+++ backend/sfx/request_test.go
@@ -1,36 +1,27 @@
 package sfx
 
 import (
-	"errors"
 	"fmt"
 	"net/url"
 	"reflect"
-	"strings"
 	"testing"
 )
 
-const mockTimestamp = "2017-10-27T10:49:40-04:00"
-
-func TestNewMultipleObjectsRequest(t *testing.T) {
+func TestNewSFXRequest(t *testing.T) {
 	var tests = []struct {
 		querystring   url.Values
 		expectedError error
 	}{
-		{map[string][]string{}, errors.New("could not parse required request body params from querystring: no valid querystring values to parse")},
-		{map[string][]string{"rft.genre": {"podcast"}}, errors.New("could not parse required request body params from querystring: genre is not valid: genre not in list of allowed genres: [podcast]")},
-		// "<" should be XML escaped properly
-		{map[string][]string{"rft.genre": {"book"}, "rft.aulast": {"<rft:"}}, nil},
-		// "&" should be XML escaped properly
-		{map[string][]string{"rft.genre": {"journal"}, "title": {"Journal of the Gilded Age & Progressive Era"}}, nil},
-		{map[string][]string{"rft.genre": {"book"}, "rft.btitle": {"dune"}}, nil},
-		// If genre is missing, automatically add genre param with value of "journal"
-		{map[string][]string{"title": {"Journal of the Gilded Age & Progressive Era"}}, nil},
+		{map[string][]string{"sid": {"genre=article&isbn=&issn=19447485&title=Community%20Development&volume=49&issue=5&date=20181020&atitle=Can%20community%20task%20groups%20learn%20from%20the%20principles%20of%20group%20therapy?&aulast=Zanbar,%20L.&spage=574&sid=EBSCO:Scopus\\\\u00ae&pid=Zanbar,%20L.edselc.2-52.0-8505573399120181020Scopus\\\\u00ae"}}, nil},
+		// TODO: Are we able to generate an error to test?
+		// {map[string][]string{}, errors.New("TODO")},
 	}
 
 	for _, testCase := range tests {
 		testName := fmt.Sprintf("%s", testCase.querystring)
 		t.Run(testName, func(t *testing.T) {
-			ans, err := NewSFXRequest(testCase.querystring)
+			// TODO: Test sfxRequest
+			_, err := NewSFXRequest(testCase.querystring)
 			if testCase.expectedError != nil {
 				if err == nil {
 					t.Errorf("NewSFXRequest returned no error, expecting '%v'", testCase.expectedError)
@@ -40,37 +31,6 @@ func TestNewMultipleObjectsRequest(t *testing.T) {
 			}
 			if err != nil && testCase.expectedError == nil {
 				t.Errorf("NewSFXRequest returned error '%v', expecting no errors", err)
-			}
-			if err == nil {
-				if !strings.HasPrefix(ans.RequestXML, `<?xml version="1.0" encoding="UTF-8"?>`) {
-					t.Errorf("requestXML isn't an XML document")
-				}
-			}
-		})
-	}
-}
-
-func TestRequestXML(t *testing.T) {
-	var tests = []struct {
-		name        string
-		tpl         sfxRequestBodyParams
-		expectedErr error
-	}{
-		{"genre=\"book\"; btitle=\"a book\"", sfxRequestBodyParams{RftValues: &map[string][]string{"genre": {"book"}, "btitle": {"a book"}}, Timestamp: mockTimestamp, Genre: "book"}, nil},
-		{"[empty request body]", sfxRequestBodyParams{}, errors.New("error")},
-		{"genre=\"<rft:\"", sfxRequestBodyParams{RftValues: &map[string][]string{"genre": {"<rft:"}}, Timestamp: mockTimestamp, Genre: "book"}, errors.New("error")},
-	}
-
-	for _, testCase := range tests {
-		t.Run(testCase.name, func(t *testing.T) {
-			actualXML, err := requestXML(testCase.tpl)
-			if testCase.expectedErr == nil && !strings.HasPrefix(actualXML, `<?xml version="1.0" encoding="UTF-8"?>`) {
-				t.Errorf("toRequestXML didn't return an XML document")
-			}
-			if testCase.expectedErr != nil {
-				if err == nil {
-					t.Errorf("toRequestXML err was '%v', expecting '%v'", err, testCase.expectedErr)
-				}
 			}
 		})
 	}
@@ -97,44 +57,3 @@ func TestFilterOpenURLParams(t *testing.T) {
 		})
 	}
 }
-
-func TestParseMultipleObjectsRequestParams(t *testing.T) {
-	var testCases = []struct {
-		queryString   map[string][]string
-		expected      *map[string][]string
-		expectedError error
-	}{
-		// Even though "rft." prefix is supposed to be required, many past requests
-		// have had non-prefixed equivalent params, so we've decided to allow them.
-		{map[string][]string{"genre": {"book"}}, &map[string][]string{"genre": {"book"}}, nil},
-
-		// "rft."-prefixed query params should always have priority over their non-prefixed equivalents.
-		// When we first made the change from requiring the prefix to not requiring it,
-		// we had a bug where priority was determined by ordering, so we test opposite
-		// orderings to prevent regression.
-		{map[string][]string{"genre": {"book"}, "rft.genre": {"journal", "book"}}, &map[string][]string{"genre": {"journal", "book"}}, nil},
-		{map[string][]string{"rft.genre": {"journal", "book"}, "genre": {"book"}}, &map[string][]string{"genre": {"journal", "book"}}, nil},
-
-		{map[string][]string{"genre": {"podcast"}}, nil, errors.New("error")},
-	}
-
-	for _, testCase := range testCases {
-		testname := fmt.Sprintf("%s", testCase.queryString)
-
-		t.Run(testname, func(t *testing.T) {
-			params, err := parseRequestParams(testCase.queryString)
-			actual := params.RftValues
-			if !reflect.DeepEqual(actual, testCase.expected) {
-				t.Errorf("parseRequestParams returned '%v', expecting '%v'", actual, testCase.expected)
-			}
-			if testCase.expectedError != nil {
-				if err == nil {
-					t.Errorf("parseRequestParams err was '%v', expecting '%v'", err, testCase.expectedError)
-				}
-			}
-		})
-	}
-}
-
-// func (c SFXRequest) Do() (body string, err error) {
-// func Init(qs url.Values) (SFXRequest *SFXRequest, err error) {
