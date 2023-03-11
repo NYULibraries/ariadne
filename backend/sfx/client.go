6597e1c5f659d67d4707206c938652a91e46165b
David <da70@nyu.edu>
2023-03-10 19:07:02 -0500

    backend/sfx/: remove all genre-related stuff

--- backend/sfx/client.go
+++ backend/sfx/client.go
@@ -2,16 +2,10 @@ package sfx
 
 import (
 	"encoding/xml"
-	"fmt"
 )
 
 // SFX service URL
 const DefaultSFXURL = "http://sfx.library.nyu.edu/sfxlcl41"
-
-// "Add support for OpenURLs with no genre parameter"
-// https://nyu-lib.monday.com/boards/765008773/pulses/4036893558
-// If genre unspecified, default genre to 'journal' https://github.com/team-umlaut/umlaut/blob/b954895e0aa0a7cd0a9ec6bb716c1886c813601e/app/service_adaptors/sfx.rb#L642
-const defaultGenre = "journal"
 
 var sfxURL = DefaultSFXURL
 
@@ -23,36 +17,7 @@ func SetSFXURL(dependencyInjectedURL string) {
 	sfxURL = dependencyInjectedURL
 }
 
-// Source: https://github.com/NYULibraries/umlaut/blob/105593ef5237f5b4b3de76b091c73dfc9e3e722f/config/locales/en.yml#L28-L43
-func genresList() (genresList map[string]bool) {
-	genresList = map[string]bool{
-		"article":      true,
-		"book":         true,
-		"bookitem":     true,
-		"conference":   true,
-		"dissertation": true,
-		"document":     true,
-		"issue":        true,
-		"journal":      true,
-		"preprint":     true,
-		"proceeding":   true,
-		"report":       true,
-		"unknown":      true,
-	}
-
-	return
-}
-
 // Validate XML, by marshalling and checking for a blank error
 func isValidXML(data []byte) bool {
 	return xml.Unmarshal(data, new(interface{})) == nil
-}
-
-// Only return a valid genre that has been allowed by the OpenURL spec
-func validGenre(genre []string) (string, error) {
-	validGenres := genresList()
-	if len(genre) > 0 && validGenres[genre[0]] {
-		return genre[0], nil
-	}
-	return "", fmt.Errorf("genre not in list of allowed genres: %v", genre)
 }
--- backend/sfx/client_test.go
+++ backend/sfx/client_test.go
@@ -1,7 +1,6 @@
 package sfx
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"testing"
@@ -24,35 +23,6 @@ func TestIsValidXML(t *testing.T) {
 			ans := isValidXML(data)
 			if ans != tt.expected {
 				t.Errorf("isValidXML returned '%v', expecting '%v'", ans, tt.expected)
-			}
-		})
-	}
-}
-
-func TestValidGenre(t *testing.T) {
-	var tests = []struct {
-		genre       []string
-		expected    string
-		expectedErr error
-	}{
-		{[]string{"book", "book"}, "book", nil},
-		{[]string{"journal", "book"}, "journal", nil},
-		{[]string{"book"}, "book", nil},
-		{[]string{"unknown"}, "unknown", nil},
-		{[]string{"wrong"}, "", errors.New("error")},
-	}
-
-	for _, tt := range tests {
-		testname := fmt.Sprintf("%s", tt.genre)
-		t.Run(testname, func(t *testing.T) {
-			ans, err := validGenre(tt.genre)
-			if ans != tt.expected {
-				t.Errorf("validGenre returned '%v', expecting '%v'", ans, tt.expected)
-			}
-			if tt.expectedErr != nil {
-				if err == nil {
-					t.Errorf("validGenre err was '%v', expecting '%v'", err, tt.expectedErr)
-				}
 			}
 		})
 	}
