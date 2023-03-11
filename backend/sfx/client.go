f6a45d0ba7225266fbf6cf50942b2e30b04c2154
David <da70@nyu.edu>
2023-03-10 19:09:59 -0500

    backend/sfx/client: remove isValidXML

--- backend/sfx/client.go
+++ backend/sfx/client.go
@@ -1,9 +1,5 @@
 package sfx
 
-import (
-	"encoding/xml"
-)
-
 // SFX service URL
 const DefaultSFXURL = "http://sfx.library.nyu.edu/sfxlcl41"
 
@@ -15,9 +11,4 @@ func Do(request *SFXRequest) (*SFXResponse, error) {
 
 func SetSFXURL(dependencyInjectedURL string) {
 	sfxURL = dependencyInjectedURL
-}
-
-// Validate XML, by marshalling and checking for a blank error
-func isValidXML(data []byte) bool {
-	return xml.Unmarshal(data, new(interface{})) == nil
 }
