92ddfe17154d6563e23a4090007b537a76e36991
David <da70@nyu.edu>
2023-03-16 09:55:06 -0400

    backend/sfx/response_test.go: implement TestRemoveTarget; add helper getExpectedTargetsStringified

--- backend/sfx/response_test.go
+++ backend/sfx/response_test.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -84,17 +86,58 @@ func TestNewMultipleObjectsResponse(t *testing.T) {
 }
 
 func TestRemoveTarget(t *testing.T) {
-	var testCases []testutils.TestCase
-	err := json.Unmarshal(testutils.TestCasesJSON, &testCases)
-	if err != nil {
-		panic(fmt.Sprintf("Error reading test cases file: %s", err))
+	targetURLsToRemove := []string{
+		"http://library.nyu.edu/ask/",
+		"http://proxy.library.nyu.edu/login\\\\?url=http://www.newyorker.com/archive",
 	}
 
-	//for _, testCase := range testCases {
-	//	fakeHTTPResponse := makeFakeHTTPResponse("")
-	//	fakeSFXResponse := makeFakeSFXResponse(fakeHTTPResponse)
-	//	fakeSFXResponse.RemoveTarget("http://library.nyu.edu/ask/")
-	//}
+	for _, testCase := range testutils.TestCases {
+		for _, targetURLToRemove := range targetURLsToRemove {
+			sfxFakeResponseFixture, err := testutils.GetSFXFakeResponse(testCase)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			fakeSFXResponse := makeFakeSFXResponse(makeFakeHTTPResponse(sfxFakeResponseFixture))
+
+			// Generate stringified expected targets slice
+			originalTargetsStringified := fmt.Sprintf("%v", (*(*fakeSFXResponse.XMLResponseBody.ContextObject)[0].SFXContextObjectTargets)[0].Targets)
+			expectedTargetsStringified := getExpectedTargetsStringified(originalTargetsStringified, targetURLToRemove)
+			fakeSFXResponse.RemoveTarget(targetURLToRemove)
+
+			// Generate stringified targets slice that should now have `targetURLToRemove` removed
+			newTargetsStringified := fmt.Sprintf("%v", (*(*fakeSFXResponse.XMLResponseBody.ContextObject)[0].SFXContextObjectTargets)[0].Targets)
+
+			if newTargetsStringified != expectedTargetsStringified {
+				t.Errorf(
+					fmt.Sprintf(
+						"%s: expected %s; got %s",
+						testCase.Name,
+						expectedTargetsStringified,
+						newTargetsStringified,
+					),
+				)
+			}
+		}
+	}
+}
+
+func getExpectedTargetsStringified(targetsStringified string, targetURLToRemove string) string {
+	// Example string matched by this regexp: {ASK_A_LIBRARIAN_LCL Ask a Librarian http://library.nyu.edu/ask/  no 0xc00000ed50}
+	targetURLRegexp := regexp.MustCompile("{[^}]*" + targetURLToRemove + "[^}]*}")
+	expectedTargetsStringified := targetURLRegexp.ReplaceAllString(targetsStringified, "")
+
+	// Clean up whitespace
+	// ...when removed target came after another target
+	expectedTargetsStringified = strings.ReplaceAll(expectedTargetsStringified, "}  ", "} ")
+	// ...when removed target came before another target
+	expectedTargetsStringified = strings.ReplaceAll(expectedTargetsStringified, "  {", " {")
+	// ...when removed target was at the end of the slice
+	expectedTargetsStringified = strings.ReplaceAll(expectedTargetsStringified, " ]", "]")
+	// ...when removed target was at the beginning of the slice
+	expectedTargetsStringified = strings.ReplaceAll(expectedTargetsStringified, "[ ", "[")
+
+	return expectedTargetsStringified
 }
 
 func makeFakeHTTPResponse(body string) *http.Response {
