d3a1cec96cc40e35740e3d3e2a3abf5b5486b5ca
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-16 17:16:32 -0400

    frontend/: Run statusembed page script conditionally in production only

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
+window.IS_PROD_ENV = process.env.NODE_ENV === 'production';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
