345bfeff4531eeb09e3eedf17caf95020a4d2229
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-20 15:22:24 -0400

    frontend/src/components/List/: Fix vertical alignment in list items;update jest snapshot

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -7,6 +7,7 @@ const List = ({ links, loading }) => {
             <div className="empty" style={emptyStyle}></div>
         ) :
             <div className="list-group">
+                <div className="list-group-item list-group-item-action flex-column border-0"></div>
                 {links?.map((link, idx) => (
                     <div key={idx} className="list-group-item list-group-item-action flex-column border-0">
                         <div className="row">
