6fb0535bb349fcf3fd75b67f289ac8bab6a29d77
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-03-23 17:14:28 -0400

    frontend/src/components/ChatWidget/: Update css styles

--- frontend/src/components/ChatWidget/ChatWidget.js
+++ frontend/src/components/ChatWidget/ChatWidget.js
@@ -17,29 +17,32 @@ const ChatWidget = () => {
     return (
         <>
             <aside id="chat_widget" tabIndex="-1">
-                {!showChat && (<button className="button chat-tab ss-chat" onClick={handleChatToggle}>
-                    Chat with us
-                </button>)}
-                <div className="chat-frame-wrap">
-                    {showChat && (
-                        <>
-                            <button
-                                className="chat-close ss-icon js-toggle-chat"
-                                title="Close chat window"
-                                aria-label="Close chat window"
-                                onClick={handleChatToggle}
-                            >
-                                X
-                            </button>
-                            <div className="libraryh3lp" data-lh3-jid="nyu-aal-chat@chat.libraryh3lp.com">
-                                <iframe
-                                    className="chat-iframe"
-                                    title="Ask a Librarian chat"
-                                    src="https://libraryh3lp.com/chat/nyu-aal-chat@chat.libraryh3lp.com?skin=23106&amp;referer=https%3A%2F%2Flibrary.nyu.edu%2F"
-                                ></iframe>
-                            </div>
-                        </>
+                <div className="chat-container">
+                    {!showChat && (
+                        <button className="button chat-tab ss-chat chat-button" onClick={handleChatToggle}>
+                            Chat with us
+                        </button>
                     )}
+                    <div className="chat-frame-wrap">
+                        {showChat && (
+                            <>
+                                <button
+                                    className="chat-close ss-icon js-toggle-chat"
+                                    title="Close chat window"
+                                    aria-label="Close chat window"
+                                    onClick={handleChatToggle}
+                                >
+                                    &times;
+                                </button>
+                                <div className="libraryh3lp" data-lh3-jid="nyu-aal-chat@chat.libraryh3lp.com">
+                                    <iframe
+                                        title="Ask a Librarian chat"
+                                        src="https://libraryh3lp.com/chat/nyu-aal-chat@chat.libraryh3lp.com?skin=23106&amp;referer=https%3A%2F%2Flibrary.nyu.edu%2F"
+                                    ></iframe>
+                                </div>
+                            </>
+                        )}
+                    </div>
                 </div>
             </aside>
         </>
