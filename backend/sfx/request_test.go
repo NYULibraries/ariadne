060222a86292047a5252d123ef8ee5b0621f2a6c
David <da70@nyu.edu>
2023-03-28 17:12:41 -0400

    backend/sfx/request_test.go: rename dumpedHTTPRequest -> expectedDumpedHTTPRequest

--- backend/sfx/request_test.go
+++ backend/sfx/request_test.go
@@ -10,10 +10,10 @@ import (
 
 func TestNewSFXRequest(t *testing.T) {
 	var tests = []struct {
-		name              string
-		dumpedHTTPRequest string
-		expectedError     error
-		queryString       string
+		name                      string
+		expectedDumpedHTTPRequest string
+		expectedError             error
+		queryString               string
 	}{
 		{
 			// This request as-is was causing SFX to return a "XSS violation occured [sic]."
@@ -23,7 +23,7 @@ func TestNewSFXRequest(t *testing.T) {
 			// NOTE: This is the can-community-task-groups-learn-from-the-principles-of-group-therapy
 			// test case from backend/testutils/testdata/test-cases.json.
 			name: "Trouble-causing `sid`",
-			dumpedHTTPRequest: `GET /sfxlcl41?atitle=Can+community+task+groups+learn+from+the+principles+of+group+therapy%3F&aulast=Zanbar%2C+L.&date=20181020&genre=article&isbn=&issn=19447485&issue=5&pid=Zanbar%2C+L.edselc.2-52.0-8505573399120181020Scopus%5C%C2%AE&rfr_id=EBSCO%3AScopus%5C%C2%AE&sfx.doi_url=http%3A%2F%2Fdx.doi.org&sfx.response_type=multi_obj_xml&spage=574&title=Community+Development&url_ctx_fmt=info%3Aofi%2Ffmt%3Axml%3Axsd%3Actx&volume=49 HTTP/1.1
+			expectedDumpedHTTPRequest: `GET /sfxlcl41?atitle=Can+community+task+groups+learn+from+the+principles+of+group+therapy%3F&aulast=Zanbar%2C+L.&date=20181020&genre=article&isbn=&issn=19447485&issue=5&pid=Zanbar%2C+L.edselc.2-52.0-8505573399120181020Scopus%5C%C2%AE&rfr_id=EBSCO%3AScopus%5C%C2%AE&sfx.doi_url=http%3A%2F%2Fdx.doi.org&sfx.response_type=multi_obj_xml&spage=574&title=Community+Development&url_ctx_fmt=info%3Aofi%2Ffmt%3Axml%3Axsd%3Actx&volume=49 HTTP/1.1
 Host: sfx.library.nyu.edu`,
 			expectedError: nil,
 			queryString:   "genre=article&isbn=&issn=19447485&title=Community%20Development&volume=49&issue=5&date=20181020&atitle=Can%20community%20task%20groups%20learn%20from%20the%20principles%20of%20group%20therapy?&aulast=Zanbar,%20L.&spage=574&sid=EBSCO:Scopus\\®&pid=Zanbar,%20L.edselc.2-52.0-8505573399120181020Scopus\\®",
@@ -31,7 +31,7 @@ Host: sfx.library.nyu.edu`,
 		{
 			// This is the `history-today` test case.
 			name: "`date` query param value is empty",
-			dumpedHTTPRequest: `GET /sfxlcl41?atitle=&aulast=&date=&genre=article&isbn=&issn=00182753&issue=&pid=Academic+Search+Complete+--+Publications&rfr_id=EBSCO%3AAcademic+Search+Complete+--+Publications&sfx.doi_url=http%3A%2F%2Fdx.doi.org&sfx.ignore_date_threshold=1&sfx.response_type=multi_obj_xml&sfx.show_availability=1&spage=&title=History+Today&url_ctx_fmt=info%3Aofi%2Ffmt%3Axml%3Axsd%3Actx&volume= HTTP/1.1
+			expectedDumpedHTTPRequest: `GET /sfxlcl41?atitle=&aulast=&date=&genre=article&isbn=&issn=00182753&issue=&pid=Academic+Search+Complete+--+Publications&rfr_id=EBSCO%3AAcademic+Search+Complete+--+Publications&sfx.doi_url=http%3A%2F%2Fdx.doi.org&sfx.ignore_date_threshold=1&sfx.response_type=multi_obj_xml&sfx.show_availability=1&spage=&title=History+Today&url_ctx_fmt=info%3Aofi%2Ffmt%3Axml%3Axsd%3Actx&volume= HTTP/1.1
 Host: sfx.library.nyu.edu`,
 			expectedError: nil,
 			queryString:   "genre=article&isbn=&issn=00182753&title=History%20Today&volume=&issue=&date=&atitle=&aulast=&spage=&sid=EBSCO:Academic%20Search%20Complete%20--%20Publications&pid=Academic%20Search%20Complete%20--%20Publications",
@@ -46,7 +46,7 @@ Host: sfx.library.nyu.edu`,
 		//
 		// {
 		//   name:              "",
-		//	 dumpedHTTPRequest: "",
+		//	 expectedDumpedHTTPRequest: "",
 		//	 expectedError:     errors.New(""),
 		//	 queryString:       string,
 		// },
