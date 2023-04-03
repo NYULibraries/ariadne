257ac4a43ce08d89af1af7ff52f02694b61c1e6e
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2023-04-03 18:46:44 -0400

    frontend/src/aux/institutionInfo.js: Add punctuation to alt text

--- frontend/src/aux/institutionInfo.js
+++ frontend/src/aux/institutionInfo.js
@@ -5,19 +5,19 @@ const bannerInstitutionInfo = deepFreeze({
         logo: 'https://cdn.library.nyu.edu/images/nyulibraries-logo.svg',
         link: 'https://library.nyu.edu',
         imgClass: 'image',
-        altLibraryLogoImageText: 'NYU Libraries homepage'
+        altLibraryLogoImageText: 'NYU Libraries homepage.'
     },
     nyuad: {
         logo: `/images/abudhabi-logo-color.svg`,
         link: 'https://nyuad.nyu.edu/en/library.html',
         imgClass: 'image white-bg',
-        altLibraryLogoImageText: 'NYU Abu Dhabi Library homepage'
+        altLibraryLogoImageText: 'NYU Abu Dhabi Library homepage.'
     },
     nyush: {
         logo: `/images/shanghai-logo-color.svg`,
         link: 'https://shanghai.nyu.edu/academics/library',
         imgClass: 'image white-bg',
-        altLibraryLogoImageText: 'NYU Shanghai Library homepage'
+        altLibraryLogoImageText: 'NYU Shanghai Library homepage.'
     }
 });
 
