35ffd9ffa629f08d7800ab68b1eacb2ca25f28ee
David <da70@nyu.edu>
2023-04-03 21:57:10 -0400

    backend/log/: add log levels vars and SetLevel function

--- backend/log/log.go
+++ backend/log/log.go
@@ -3,13 +3,26 @@ package log
 import (
 	"fmt"
 	"golang.org/x/exp/slog"
+	"math"
 	"os"
+	"reflect"
 )
 
+type Level int
+
+var (
+	LevelDebug    = Level(reflect.ValueOf(slog.LevelDebug).Int())
+	LevelInfo     = Level(reflect.ValueOf(slog.LevelInfo).Int())
+	LevelWarn     = Level(reflect.ValueOf(slog.LevelWarn).Int())
+	LevelError    = Level(reflect.ValueOf(slog.LevelWarn).Int())
+	LevelDisabled = Level(math.MaxInt)
+)
+
+var programLevel = new(slog.LevelVar)
 var slogger *slog.Logger
 
 func init() {
-	handler := slog.HandlerOptions{Level: slog.LevelInfo}.NewJSONHandler(os.Stdout)
+	handler := slog.HandlerOptions{Level: programLevel}.NewJSONHandler(os.Stdout)
 	slog.SetDefault(slog.New(handler))
 
 	slogger = slog.New(handler)
@@ -26,4 +39,8 @@ func Error(message string, args ...interface{}) {
 
 func Info(message string, args ...interface{}) {
 	slogger.Info(message, args...)
+}
+
+func SetLevel(level Level) {
+	programLevel.Set(slog.Level(level))
 }
