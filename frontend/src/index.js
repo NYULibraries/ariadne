de44803c874fd05e2a683a42b184dfba45b7a425
Eric Griffis <eric.griffis@nyu.edu>
2023-04-04 21:51:36 -0400

    Frontend - inline scripts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,3 +9,21 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// statuspage embed
+(function () {
+  var env = '%REACT_APP_ENV%';
+  if (env === 'production') {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.library.nyu.edu/statuspage-embed/index.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+})();
+
+// libraryh3lp chat widget
+(function () {
+  var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+  x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?7516";
+  var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+})();
